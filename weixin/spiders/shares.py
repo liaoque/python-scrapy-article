a8940cdaee17edf4b7640d3e46c0b5382131453c
mazuqiang <844596330@qq.com>
2021-12-18 21:32:00 +0800

    跑日期

--- weixin/spiders/shares.py
+++ weixin/spiders/shares.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import scrapy
 import json
 from scrapy.loader import ItemLoader
@@ -19,17 +21,14 @@ class Shares(scrapy.Spider):
     db = None
     cursor = None
 
-    def get_url(self, code):
+    def get_url(self, code, days):
+        if days <= 0:
+            days = 1000000
         return 'https://push2his.eastmoney.com/api/qt/stock/kline/get?secid=' + \
-               str(code) + '&cb=&klt=101&fqt=0&lmt=1000000&end=20500101&iscca=1&fields1=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13&fields2=f51,f52,f53,f54,f55,f56,f57,f58'
+               str(code) + '&cb=&klt=101&fqt=0&lmt=' + str(days) + \
+               '&end=20500101&iscca=1&fields1=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13&fields2=f51,f52,f53,f54,f55,f56,f57,f58'
 
-    def __init__(self, name=None, **kwargs):
-        super().__init__(name=name, **kwargs)
-
-
-
-    # http://11.push2.eastmoney.com/api/qt/clist/get?cb=&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1584074266443"
-    def start_requests(self):
+    def connect(self):
         if self.db == None:
             self.db = MySQLdb.connect(host=self.settings.get('MYSQL_HOST'),
                                       user=self.settings.get('MYSQL_USER'),
@@ -37,20 +36,24 @@ class Shares(scrapy.Spider):
                                       database=self.settings.get('MYSQL_DBNAME'),
                                       charset='utf8mb4')
             self.cursor = self.db.cursor()
+
+    # http://11.push2.eastmoney.com/api/qt/clist/get?cb=&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1584074266443"
+    def start_requests(self):
+        self.connect()
         results = self.findStoks()
         for item in results:
             code = item[0]
+            days = 0
 
             stock_info = self.findStokByCode(code)
-            print(stock_info)
-            return
-
+            if stock_info:
+                days = (datetime.now() - stock_info[2]).days
             area_id = item[2]
             if area_id == 1:
                 s_code = "1." + code
             else:
                 s_code = "0." + code
-            url = self.get_url(s_code)
+            url = self.get_url(s_code, days)
             yield scrapy.Request(url,
                                  headers=self.headers,
                                  dont_filter=True,
