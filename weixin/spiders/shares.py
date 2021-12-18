5724629fca1406b902bb1574cce874d0308f4bcd
mazuqiang <844596330@qq.com>
2021-12-18 20:20:13 +0800

    跑日期

--- weixin/spiders/shares.py
+++ weixin/spiders/shares.py
@@ -23,8 +23,7 @@ class Shares(scrapy.Spider):
         return 'https://push2his.eastmoney.com/api/qt/stock/kline/get?secid=' + str(code) + '&cb=&klt=101&fqt=0&lmt=1000000&end=20500101&iscca=1&fields1=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13&fields2=f51,f52,f53,f54,f55,f56,f57,f58'
 
 # http://11.push2.eastmoney.com/api/qt/clist/get?cb=&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1584074266443"
-    def __init__(self, name=None, **kwargs):
-        super().__init__(name=name, **kwargs)
+    def start_requests(self):
         db = MySQLdb.connect(host=self.settings.get('MYSQL_HOST'),
                              user=self.settings.get('MYSQL_USER'),
                              password=self.settings.get('MYSQL_PASSWORD'),
