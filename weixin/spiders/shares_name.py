2196e96ab72c705e6336539bcb92d4afbfd0f040
mazuqiang <844596330@qq.com>
2021-12-18 19:59:14 +0800

    跑保存股票名字

--- weixin/spiders/shares_name.py
+++ weixin/spiders/shares_name.py
@@ -15,16 +15,21 @@ class Shares_name(scrapy.Spider):
     allowed_domains = ['.eastmoney.com']
     start_urls = []
     area_map = {
-        "SH": "m:1 t:2,m:1 t:23",
-        "SZ": "m:0 t:6,m:0 t:80",
-        "BJ": "m:0 t:81 s:2048",
+        "SH": "m:1+t:2,m:1+t:23",
+        "SZ": "m:0+t:6,m:0+t:80",
+        "BJ": "m:0+t:81+s:2048",
     }
     headers = {
         "HOST": "push2.eastmoney.com"
     }
 
     def get_url(self, n, area):
-        return "https://75.push2.eastmoney.com/api/qt/clist/get?cb=&pn=" + str(n) + "&pz=200&po=1&np=1&" \
+        # "http://87.push2.eastmoney.com/api/qt/clist/get?cb=&pn=1&pz=20&po=0&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f12&fs=m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1639825644542"
+        # "http://87.push2.eastmoney.com/api/qt/clist/get?cb=&pn=1&pz=20&po=0&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f12&fs=m:0+t:6,m:0+t:80&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1639825644552"
+        # "http://87.push2.eastmoney.com/api/qt/clist/get?cb=&pn=1&pz=20&po=0&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f12&fs=m:0+t:81+s:2048&fields=f12,f13,f14&_=1639825644572"
+        # "http://87.push2.eastmoney.com/api/qt/clist/get?cb=&pn=3&pz=20&po=0&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f12&fs=m:0+t:81+s:2048&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1639825644581"
+        # "http://75.push2.eastmoney.com/api/qt/clist/get?cb=&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f12&fs=m:1%20t:2,m:1%20t:23&fields=f12,f13,f14&_=1639813284425"
+        return "https://75.push2.eastmoney.com/api/qt/clist/get?cb=&pn=" + str(n) + "&pz=200&po=0&np=1&" \
                                                                                     "ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f12&fs=" + area + "&fields=f12,f13,f14&_=1639813284425"
 
     # http://11.push2.eastmoney.com/api/qt/clist/get?cb=&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1584074266443"
@@ -48,7 +53,6 @@ class Shares_name(scrapy.Spider):
         while n <= total:
             url = self.get_url(n, self.area_map[area])
             headers['area'] = area
-            print(url)
             n = n + 1
             yield scrapy.Request(url, headers=headers, callback=self.parse_content,dont_filter=True)
 
