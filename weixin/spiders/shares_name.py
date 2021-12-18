f63ae571fc161e58dd33fabb3279e269363cf436
mazuqiang <844596330@qq.com>
2021-12-18 17:36:44 +0800

    跑保存股票名字

--- weixin/spiders/shares_name.py
+++ weixin/spiders/shares_name.py
@@ -28,7 +28,7 @@ class Shares_name(scrapy.Spider):
         area = self.area_map["SZ"]
         self.headers['area'] = "SZ"
         while n <= self.total:
-            url = "https://75.push2.eastmoney.com/api/qt/clist/get?cb=&pn=" + n + "&pz=200&po=1&np=1&" \
+            url = "https://75.push2.eastmoney.com/api/qt/clist/get?cb=&pn=" + str(n) + "&pz=200&po=1&np=1&" \
                   "ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f12&fs=" + area + "&fields=f12,f13,f14&_=1639813284425"
             print(url)
             n = n + 1
@@ -38,7 +38,7 @@ class Shares_name(scrapy.Spider):
         area = self.area_map["SH"]
         self.headers['area'] = "SH"
         while n <= self.total:
-            url = "https://75.push2.eastmoney.com/api/qt/clist/get?cb=&pn=" + n + "&pz=200&po=1&np=1&" \
+            url = "https://75.push2.eastmoney.com/api/qt/clist/get?cb=&pn=" + str(n) + "&pz=200&po=1&np=1&" \
                   "ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f12&fs=" + area + "&fields=f12,f13,f14&_=1639813284425"
             n = n + 1
             yield scrapy.Request(url, headers=self.headers, callback=self.parse_content)
@@ -47,7 +47,7 @@ class Shares_name(scrapy.Spider):
         area = self.area_map["BJ"]
         self.headers['area'] = "BJ"
         while n <= self.total:
-            url = "https://75.push2.eastmoney.com/api/qt/clist/get?cb=&pn=" + n + "&pz=200&po=1&np=1&" \
+            url = "https://75.push2.eastmoney.com/api/qt/clist/get?cb=&pn=" + str(n) + "&pz=200&po=1&np=1&" \
                   "ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f12&fs=" + area + "&fields=f12,f13,f14&_=1639813284425"
             n = n +1
             yield scrapy.Request(url, headers=self.headers, callback=self.parse_content)
