ac406b1334e9e3fb7b071088f2438a7553f0d404
mazuqiang <844596330@qq.com>
2021-12-18 17:54:58 +0800

    跑保存股票名字

--- weixin/spiders/shares_name.py
+++ weixin/spiders/shares_name.py
@@ -28,15 +28,15 @@ class Shares_name(scrapy.Spider):
 
     # http://11.push2.eastmoney.com/api/qt/clist/get?cb=&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1584074266443"
     def start_requests(self):
-        self.total = n = 1
-
         self.headers['area'] = "SZ"
         url = self.get_url(1, self.area_map["SZ"])
         yield scrapy.Request(url, headers=self.headers, callback=self.parse_each)
 
+        self.headers['area'] = "SH"
         url = self.get_url(1, self.area_map["SH"])
         yield scrapy.Request(url, headers=self.headers, callback=self.parse_each)
 
+        self.headers['area'] = "BJ"
         url = self.get_url(1, self.area_map["BJ"])
         yield scrapy.Request(url, headers=self.headers, callback=self.parse_each)
 
@@ -47,7 +47,7 @@ class Shares_name(scrapy.Spider):
         self.headers['area'] = area
         area = self.area_map[area]
         n = 1
-        while n <= self.total:
+        while n <= total:
             url = self.get_url(n, area)
             n = n + 1
             yield scrapy.Request(url, headers=self.headers, callback=self.parse_content)
