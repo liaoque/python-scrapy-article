7e8be86e2279c169faad3d27d0ba2635da62761f
mazuqiang <844596330@qq.com>
2021-12-18 19:16:48 +0800

    跑保存股票名字

--- weixin/spiders/shares_name.py
+++ weixin/spiders/shares_name.py
@@ -6,6 +6,7 @@ import re
 
 import weixin.shares.items as SharesItems
 import time
+import copy
 
 
 class Shares_name(scrapy.Spider):
@@ -28,29 +29,28 @@ class Shares_name(scrapy.Spider):
 
     # http://11.push2.eastmoney.com/api/qt/clist/get?cb=&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1584074266443"
     def start_requests(self):
-        self.headers['area'] = "SZ"
-        url = self.get_url(1, self.area_map["SZ"])
-        yield scrapy.Request(url, headers=self.headers, callback=self.parse_each)
+        self.init_area_request("SH")
+        self.init_area_request("SZ")
+        self.init_area_request("BJ")
 
-        self.headers['area'] = "SH"
-        url = self.get_url(1, self.area_map["SH"])
-        yield scrapy.Request(url, headers=self.headers, callback=self.parse_each)
-
-        self.headers['area'] = "BJ"
-        url = self.get_url(1, self.area_map["BJ"])
-        yield scrapy.Request(url, headers=self.headers, callback=self.parse_each)
+    def init_area_request(self, area):
+        headers = copy.deepcopy(self.headers)
+        headers['area'] = area
+        url = self.get_url(1, self.area_map[area])
+        yield scrapy.Request(url, headers=headers, callback=self.parse_each)
 
     def parse_each(self, response):
         result = json.loads(response.text)
         total = result["data"]["total"] / 200 + 1
         area = response.request.headers.getlist('area')[0].decode("UTF-8")
         n = 1
+        headers = copy.deepcopy(self.headers)
         while n <= total:
             url = self.get_url(n, self.area_map[area])
-            self.headers['area'] = area
+            headers['area'] = area
             print(url)
             n = n + 1
-            yield scrapy.Request(url, headers=self.headers, callback=self.parse_content,dont_filter=True)
+            yield scrapy.Request(url, headers=headers, callback=self.parse_content,dont_filter=True)
 
     def parse_content(self, response):
         result = json.loads(response.text)
