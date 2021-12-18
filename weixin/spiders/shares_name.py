930c98e56c73756b3c108c4f8e5a1fd4c636fcc3
mazuqiang <844596330@qq.com>
2021-12-18 17:47:34 +0800

    跑保存股票名字

--- weixin/spiders/shares_name.py
+++ weixin/spiders/shares_name.py
@@ -22,35 +22,34 @@ class Shares_name(scrapy.Spider):
         "HOST": "push2.eastmoney.com"
     }
 
+    def get_url(self, n, area):
+        return "https://75.push2.eastmoney.com/api/qt/clist/get?cb=&pn=" + str(n) + "&pz=200&po=1&np=1&" \
+                                                                                    "ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f12&fs=" + area + "&fields=f12,f13,f14&_=1639813284425"
+
     # http://11.push2.eastmoney.com/api/qt/clist/get?cb=&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1584074266443"
     def start_requests(self):
         self.total = n = 1
-        area = self.area_map["SZ"]
+
         self.headers['area'] = "SZ"
-        while n <= self.total:
-            url = "https://75.push2.eastmoney.com/api/qt/clist/get?cb=&pn=" + str(n) + "&pz=200&po=1&np=1&" \
-                  "ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f12&fs=" + area + "&fields=f12,f13,f14&_=1639813284425"
-            print(url)
-            n = n + 1
-            yield scrapy.Request(url, headers=self.headers, callback=self.parse_content)
+        url = self.get_url(1, self.area_map["SZ"])
+        yield scrapy.Request(url, headers=self.headers, callback=self.parse_each)
 
-        self.total = n = 1
-        area = self.area_map["SH"]
-        self.headers['area'] = "SH"
-        while n <= self.total:
-            url = "https://75.push2.eastmoney.com/api/qt/clist/get?cb=&pn=" + str(n) + "&pz=200&po=1&np=1&" \
-                  "ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f12&fs=" + area + "&fields=f12,f13,f14&_=1639813284425"
-            n = n + 1
-            yield scrapy.Request(url, headers=self.headers, callback=self.parse_content)
+        url = self.get_url(1, self.area_map["SH"])
+        yield scrapy.Request(url, headers=self.headers, callback=self.parse_each)
 
-        self.total = n = 1
-        area = self.area_map["BJ"]
-        self.headers['area'] = "BJ"
+        url = self.get_url(1, self.area_map["BJ"])
+        yield scrapy.Request(url, headers=self.headers, callback=self.parse_each)
+
+    def parse_each(self, response):
+        result = json.loads(response.text)
+        total = result["data"]["total"] / 200 + 1
+        area = response.request.headers.getlist('area')[0]
+        self.headers['area'] = area
+        area = self.area_map[area]
+        n = 1
         while n <= self.total:
-            url = "https://75.push2.eastmoney.com/api/qt/clist/get?cb=&pn=" + str(n) + "&pz=200&po=1&np=1&" \
-                  "ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f12&fs=" + area + "&fields=f12,f13,f14&_=1639813284425"
-            n = n +1
-            yield scrapy.Request(url, headers=self.headers, callback=self.parse_content)
+            url = self.get_url(n, area)
+            yield scrapy.Request(url, headers=self.headers, callback=self.parse_each)
 
     def parse_content(self, response):
         result = json.loads(response.text)
