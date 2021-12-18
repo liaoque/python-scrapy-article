9bd303a03fc935e2bdbf678c2f7124acc05e5c1a
mazuqiang <844596330@qq.com>
2021-12-18 20:49:03 +0800

    跑日期

---
+++ weixin/shares/items_date.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+# 股票抓取
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+import re
+
+
+class Items(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    id = scrapy.Field()
+    name = scrapy.Field()
+    code = scrapy.Field()
+    p_min = scrapy.Field()
+    p_max = scrapy.Field()
+    p_start = scrapy.Field()
+    p_end = scrapy.Field()
+    p_range = scrapy.Field()
+    buy_count = scrapy.Field()
+    buy_sum = scrapy.Field()
+    date_as = scrapy.Field()
+    temper_tonghuashun = scrapy.Field()
+    area_id = scrapy.Field()
+    temper_dongfangcaifu = scrapy.Field()
+    area_map = {
+        "SH": 1,
+        "SZ": 2,
+        "BJ": 3,
+    }
+
+    def save(self, cursor):
+        code = self['code'][0]
+        date_as = self['date_as'][0]
+        if self.exitsByCode(cursor, code, date_as):
+            return
+        sql = """
+            INSERT INTO mc_shares (name, code, p_min, p_max, p_start, p_end, p_range, buy_count, buy_sum, date_as)
+            VALUES (%s, %s, %s,  %s,  %s, %s, %s, %s,  %s,  %s)
+            """;
+        params = (
+            self["name"][0],
+            code,
+            int(float(self["p_min"][0]) * 100),
+            int(float(self["p_max"][0]) * 100),
+            int(float(self["p_start"][0]) * 100),
+            int(float(self["p_end"][0]) * 100),
+            int(float(self["p_range"][0]) * 100),
+            self["buy_count"][0],
+            float(self["buy_sum"][0]) * 100,
+            self["date_as"][0]
+        )
+        cursor.execute(sql, params)
+        pass
+    pass
+
+    def findByCode(self, cursor, code, date_as):
+        sql = "SELECT id FROM mc_shares  WHERE code = '%s' and date_as='%s'" % (code, date_as)
+        cursor.execute(sql);
+        return cursor
+
+    def exitsByCode(self, cursor, code, date_as):
+        sql = "SELECT id FROM mc_shares  WHERE code = '%s' and date_as='%s'" % (code, date_as)
+        cursor.execute(sql);
+        return cursor.rowcount
--- weixin/spiders/shares.py
+++ weixin/spiders/shares.py
@@ -1,5 +1,3 @@
-
-
 import scrapy
 import json
 from scrapy.loader import ItemLoader
@@ -7,50 +5,56 @@ import sys
 import MySQLdb
 import MySQLdb.cursors
 
-
-import weixin.shares.items as SharesItems
+import weixin.shares.items_date as SharesDateItems
 import time
+
 
 class Shares(scrapy.Spider):
     name = 'shares'
     allowed_domains = ['.eastmoney.com']
-    start_urls = [ ]
+    start_urls = []
     headers = {
         "HOST": "push2.eastmoney.com"
     }
+    db = None
+    cursor = None
 
     def get_url(self, code):
-        return 'https://push2his.eastmoney.com/api/qt/stock/kline/get?secid=' + str(code) + '&cb=&klt=101&fqt=0&lmt=1000000&end=20500101&iscca=1&fields1=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13&fields2=f51,f52,f53,f54,f55,f56,f57,f58'
+        return 'https://push2his.eastmoney.com/api/qt/stock/kline/get?secid=' + \
+               str(code) + '&cb=&klt=101&fqt=0&lmt=1000000&end=20500101&iscca=1&fields1=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13&fields2=f51,f52,f53,f54,f55,f56,f57,f58'
 
-# http://11.push2.eastmoney.com/api/qt/clist/get?cb=&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1584074266443"
-    def start_requests(self):
-        db = MySQLdb.connect(host=self.settings.get('MYSQL_HOST'),
+    def __init__(self, name=None, **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.db = MySQLdb.connect(host=self.settings.get('MYSQL_HOST'),
                              user=self.settings.get('MYSQL_USER'),
                              password=self.settings.get('MYSQL_PASSWORD'),
                              database=self.settings.get('MYSQL_DBNAME'),
                              charset='utf8mb4')
-        cursor = db.cursor()
-        sql = 'select code,name,area_id from mc_shares_name';
-        try:
-            # 执行SQL语句
-            cursor.execute(sql)
-            # 获取所有记录列表
-            results = cursor.fetchall()
-        except:
-            print("Error: unable to fecth data")
+        self.cursor = self.db.cursor()
 
+
+    # http://11.push2.eastmoney.com/api/qt/clist/get?cb=&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1584074266443"
+    def start_requests(self):
+        results = self.findStoks()
         for item in results:
-            print(item)
+            code = item[0]
+
+            stock_info = self.findStokByCode(code)
+            print(stock_info)
             return
-            code = item['code']
-            url = self.get_url(code)
+
+            area_id = item[2]
+            if area_id == 1:
+                s_code = "1." + code
+            else:
+                s_code = "0." + code
+            url = self.get_url(s_code)
             yield scrapy.Request(url,
                                  headers=self.headers,
                                  dont_filter=True,
                                  callback=self.parse_content)
         pass
 
-    
     def parse_content(self, response):
         result = json.loads(response.text)
         # print(result["data"]["klines"])
@@ -60,7 +64,7 @@ class Shares(scrapy.Spider):
         # text_type = response.css('.h70 .fl_l::text').extract_first();
         for item in result["data"]["klines"]:
             res = item.split(',')
-            item_loader = ItemLoader(item=SharesItems.Items())
+            item_loader = ItemLoader(item=SharesDateItems.Items())
 
             # 文档标题
             item_loader.add_value("name", result["data"]["name"])
@@ -75,6 +79,32 @@ class Shares(scrapy.Spider):
             item_loader.add_value("date_as", res[0])
             yield item_loader.load_item()
         pass
-        
-        # return item_loader.load_item()
-    pass
+
+    def findStoks(self):
+        sql = 'select code,name,area_id from mc_shares_name';
+        results = []
+        try:
+            # 执行SQL语句
+            self.cursor.execute(sql)
+            # 获取所有记录列表
+            results = self.cursor.fetchall()
+        except:
+            print("Error: unable to fecth data")
+        return results
+
+
+    def findStokByCode(self, code):
+        sql = 'SELECT code,name,date_as FROM `mc_shares` WHERE CODE = %s ORDER BY date_as DESC LIMIT 1'%(code);
+        results = []
+        try:
+            # 执行SQL语句
+            self.cursor.execute(sql)
+            # 获取所有记录列表
+            results = self.cursor.fetchone()
+        except:
+            print("Error: unable to fecth data")
+        return results
+
+    def __del__(self):
+        self.cursor.close()
+        self.db.close()
