b270d0e4b11001e2d4d9959706f638606fb59e26
mazuqiang <844596330@qq.com>
2021-12-18 17:18:46 +0800

    跑保存股票名字

--- weixin/spiders/shares_name.py
+++ weixin/spiders/shares_name.py
@@ -23,14 +23,14 @@ class Shares_name(scrapy.Spider):
     }
 
     # http://11.push2.eastmoney.com/api/qt/clist/get?cb=&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1584074266443"
-    def __init__(self, name=None, **kwargs):
-        super().__init__(name=name, **kwargs)
+    def start_requests(self):
         self.total = n = 1
         area = self.area_map["SZ"]
         self.headers['area'] = "SZ"
         while n <= self.total:
             url = "https://75.push2.eastmoney.com/api/qt/clist/get?cb=&pn=1&pz=200&po=1&np=1&" \
                   "ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f12&fs=" + area + "&fields=f12,f13,f14&_=1639813284425"
+            print(url)
             yield scrapy.Request(url, headers=self.headers, callback=self.parse_content)
 
         self.total = n = 1
