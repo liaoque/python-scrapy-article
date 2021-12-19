c73168fd9348e881e470b815f6b1d9b629d191ab
mazuqiang <844596330@qq.com>
2021-12-19 16:03:56 +0800

    执行命令

--- stock/shares/management/commands/closepoll.py
+++ stock/shares/management/commands/closepoll.py
@@ -1,13 +1,13 @@
 
 from django.core.management.base import BaseCommand, CommandError
 # from ....polls.models import Question as Poll
-from ....shares.model.shares_name import SharesName
+from shares.model.shares_name import SharesName
 # from ....shares.model.shares_kdj import SharesKdj
 # from ....shares.model.shares import Shares
 import numpy
-import talib
+# import talib
 
-
+" /bin/cp /alidata/python/python-scrapy-article/stock/* /alidata/python/python-scrapy-article-master/stock -rf"
 class Command(BaseCommand):
     help = '计算各种指标'
 
