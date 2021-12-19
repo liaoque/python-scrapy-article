1ac8ac719263dee4cebf98e6346222c94d732c3f
mazuqiang <844596330@qq.com>
2021-12-19 16:12:13 +0800

    执行命令

---
+++ stock/shares/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+# Create your models here.
+
+from .model.shares_name import SharesName
+from .model.shares import Shares
+from .model.shares_kdj import SharesKdj
+
