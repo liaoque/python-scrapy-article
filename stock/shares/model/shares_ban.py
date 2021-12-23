785d503de865ead346bf8a43f2329a7c1c583ecd
mazuqiang <844596330@qq.com>
2021-12-23 21:05:36 +0800

    shares_ban 风险股票

--- stock/shares/model/shares_ban.py
+++ stock/shares/model/shares_ban.py
@@ -9,12 +9,11 @@ class SharesBan(models.Model):
     code = models.ForeignKey(SharesName, on_delete=models.CASCADE)
     remain_avoid_cycle = models.IntegerField(default=0, help_text="剩余回避天数")
     avoid_cycle = models.FloatField(default=0, help_text="回避天数")
-    avoid_reason = models.CharField(default=20, help_text='原因')
+    avoid_reason = models.CharField(max_length=20, help_text='原因')
     date_as = models.DateField()
 
     class Meta:
         db_table = "mc_shares_ban"
-        # abstract = True
 
     def __str__(self):
-        return self.name
+        return self.code_id + ":" + self.avoid_reason
