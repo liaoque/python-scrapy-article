7e22352d91cf8a9749565b394f0e7a8510071e9f
mazuqiang <844596330@qq.com>
2021-12-26 10:14:42 +0800

    统计分析kdj

--- stock/shares/management/commands/closepoll.py
+++ stock/shares/management/commands/closepoll.py
@@ -104,7 +104,7 @@ class Command(BaseCommand):
         if len(data) == 0:
             # 当天不 需要计算
             return
-        result = np.array(SharesKdj.objects.values('date_as'))[-60:]
+        result = np.array(SharesKdj.objects.values('date_as').annotate(counts='id'))[-60:]
         print(result, len(result))
         # first, second, third, fourth, fifth = SharesKdj.objects.values('date_as')[-5:]
         # print(first, second, third, fourth, fifth)
