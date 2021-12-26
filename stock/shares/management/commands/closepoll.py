82f35db8912fc26d495fc0a026e7149767b5d969
mazuqiang <844596330@qq.com>
2021-12-26 10:16:36 +0800

    统计分析kdj

--- stock/shares/management/commands/closepoll.py
+++ stock/shares/management/commands/closepoll.py
@@ -106,8 +106,8 @@ class Command(BaseCommand):
         if len(data) == 0:
             # 当天不 需要计算
             return
-        result = np.array(SharesKdj.objects.values('date_as').annotate(counts=Count(id)))[-60:]
-        print(result, len(result))
+        first, second, third, fourth, fifth = np.array(SharesKdj.objects.values('date_as').annotate(counts=Count(id)))[-5:]
+        print(first, second, third, fourth, fifth)
         # first, second, third, fourth, fifth = SharesKdj.objects.values('date_as')[-5:]
         # print(first, second, third, fourth, fifth)
         # intersection_total = SharesKdjCompute.Compute.intersection_total(third, fourth, fifth)
