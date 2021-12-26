c4e83480208d77eba5311432d1eb1329fd6e2033
mazuqiang <844596330@qq.com>
2021-12-26 10:02:15 +0800

    统计分析kdj

--- stock/shares/management/commands/closepoll.py
+++ stock/shares/management/commands/closepoll.py
@@ -104,7 +104,8 @@ class Command(BaseCommand):
         if len(data) == 0:
             # 当天不 需要计算
             return
-        first, second, third, fourth, fifth = Shares.objects.filter(group_by='date_as')[-5:]
+        first, second, third, fourth, fifth = Shares.objects.values('date_as')[-5:]
+        print(first, second, third, fourth, fifth)
         intersection_total = SharesKdjCompute.Compute.intersection_total(third, fourth, fifth)
         intersection_today = SharesKdjCompute.Compute.intersection_today(third, fourth, fifth)
         intersection_pre = SharesKdjCompute.Compute.intersection_pre(second, third, fourth, fifth)
