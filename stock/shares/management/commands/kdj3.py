056fa3f683c918252f30e60830437b5bda6c68d8
mazuqiang <844596330@qq.com>
2022-01-01 00:43:43 +0800

    计算kdj3

--- stock/shares/management/commands/kdj3.py
+++ stock/shares/management/commands/kdj3.py
@@ -36,6 +36,6 @@ class Command(BaseCommand):
 
     def getAllDates(self):
         sql = '''
-            select date_as from mc_shares_kdj group by date_as;
+            select 1 as id, date_as from mc_shares_kdj group by date_as;
             '''
         return SharesKdjCompute.objects.raw(sql)
