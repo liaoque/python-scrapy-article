0a1d3d32854473546ac368eeab1605912ff41f2d
mazuqiang <844596330@qq.com>
2022-01-01 00:31:35 +0800

    加入日志

---
+++ stock/shares/management/commands/kdj3.py
@@ -0,0 +1,41 @@
+import numpy as np
+from datetime import datetime
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Count
+
+# from ....polls.models import Question as Poll
+from shares.model.shares_name import SharesName
+from shares.model.shares_kdj import SharesKdj
+from shares.model.shares import Shares
+from shares.model.shares_kdj_compute import SharesKdjCompute
+from shares.model.shares_kdj_compute_detail import SharesKdjComputeDetail
+# from ....shares.model.shares_kdj import SharesKdj
+# from ....shares.model.shares import Shares
+import numpy as np
+import talib
+
+" /bin/cp /alidata/python/python-scrapy-article/stock/* /alidata/python/python-scrapy-article-master/stock -rf"
+
+
+class Command(BaseCommand):
+    help = '计算各种指标'
+
+    def add_arguments(self, parser):
+        # parser.add_argument('poll_ids', nargs='+', type=int)
+        pass
+
+    def handle(self, *args, **options):
+        print("开始计算-----")
+        dateList = self.getAllDates()
+        len = len(dateList) / 5
+        while len > 0:
+            dates = dateList[len-5:5]
+            print(dates, dates[0], dates[4])
+            break
+        pass
+
+    def getAllDates(self):
+        sql = '''
+            select date from mc_shares_kdj group by date;
+            '''
+        return SharesKdjCompute.objects.raw(sql)
