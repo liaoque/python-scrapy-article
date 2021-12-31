517ded89ca0b7adc5858567fe134055d660d96f1
mazuqiang <844596330@qq.com>
2022-01-01 01:46:32 +0800

    计算kdj3

--- stock/shares/management/commands/kdj3.py
+++ stock/shares/management/commands/kdj3.py
@@ -35,7 +35,7 @@ class Command(BaseCommand):
             second = dateList[1+i].date_as
             if 5+i >= len(dateList):
                 break
-            fifth = dateList[5+i].date_as
+            fifth = dateList[4+i].date_as
             result = self.compute1(first, second, fifth)
             slen = len(result)
             print("%s-%s-%s-收益大于3的：%s"%(first, second, fifth, slen))
