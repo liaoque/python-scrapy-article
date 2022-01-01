a15a94d6a36c9c5a3d256ffef19d4f3dec28309c
mazuqiang <844596330@qq.com>
2022-01-01 21:12:12 +0800

    添加板块抓取

--- stock/shares/management/commands/kdj3.py
+++ stock/shares/management/commands/kdj3.py
@@ -35,7 +35,7 @@ class Command(BaseCommand):
         print(result)
         print("%s-%s-挑选出股票：%s个" % (first, second, len(result)))
         for item in result:
-            print("%s" % (item.code_id))
+            print('%s' % (item.code_id))
         print("---------")
 
         i = 0
@@ -70,20 +70,20 @@ class Command(BaseCommand):
     def compute1(self, first, second, fifth):
         sql = '''
             SELECT 1 as id, mc_shares_kdj.code_id,(e.p_end / c.p_end) as rate FROM `mc_shares_kdj`
-            left join (select p_end,code_id from mc_shares where date_as = "%s") c on c.code_id = mc_shares_kdj.code_id
-            left join (select p_end,code_id from mc_shares where date_as = "%s") d on d.code_id = mc_shares_kdj.code_id
-            left join (select p_end,code_id,p_max from mc_shares where date_as = "%s") e on e.code_id = mc_shares_kdj.code_id
+            left join (select p_end,code_id from mc_shares where date_as = '%s') c on c.code_id = mc_shares_kdj.code_id
+            left join (select p_end,code_id from mc_shares where date_as = '%s') d on d.code_id = mc_shares_kdj.code_id
+            left join (select p_end,code_id,p_max from mc_shares where date_as = '%s') e on e.code_id = mc_shares_kdj.code_id
             left join (select code_id,industry_code_id from mc_shares_join_industry) f on f.code_id = mc_shares_kdj.code_id
-            where j <16 and date_as = "%s" and mc_shares_kdj.code_id not in (SELECT code_id FROM `mc_shares_ban` )
-            and mc_shares_kdj.code_id not in (SELECT code FROM `mc_shares_name` where name like "%s" )
+            where j <16 and date_as = '%s' and mc_shares_kdj.code_id not in (SELECT code_id FROM `mc_shares_ban` )
+            and mc_shares_kdj.code_id not in (SELECT code FROM `mc_shares_name` where name like '%s' )
             and (mc_shares_kdj.code_id < 300000 or mc_shares_kdj.code_id > 600000)
             and mc_shares_kdj.code_id < 800000
             and  ((k - j) > 0 and (d - j) > 0) and ((k - j) <=5 and (d - j) <= 5)
             and c.p_end > d.p_end
             and f.industry_code_id in (
                 select mc_shares_industry.code_id from mc_shares_industry
-                left join (select code_id,p_end from mc_shares_industry where date_as = "%s") z on z.code_id = mc_shares_industry.code_id
-                where date_as = "%s" and z.p_end < mc_shares_industry.p_end
+                left join (select code_id,p_end from mc_shares_industry where date_as = '%s') z on z.code_id = mc_shares_industry.code_id
+                where date_as = '%s' and z.p_end < mc_shares_industry.p_end
             )
             having rate >= 1.03
         '''
@@ -92,20 +92,20 @@ class Command(BaseCommand):
     def compute2(self, first, second, fifth):
         sql = '''
             SELECT 1 as id, mc_shares_kdj.code_id,(e.p_end / c.p_end) as rate FROM `mc_shares_kdj`
-            left join (select p_end,code_id from mc_shares where date_as = "%s") c on c.code_id = mc_shares_kdj.code_id
-            left join (select p_end,code_id from mc_shares where date_as = "%s") d on d.code_id = mc_shares_kdj.code_id
-            left join (select p_end,code_id,p_max from mc_shares where date_as = "%s") e on e.code_id = mc_shares_kdj.code_id
+            left join (select p_end,code_id from mc_shares where date_as = '%s') c on c.code_id = mc_shares_kdj.code_id
+            left join (select p_end,code_id from mc_shares where date_as = '%s') d on d.code_id = mc_shares_kdj.code_id
+            left join (select p_end,code_id,p_max from mc_shares where date_as = '%s') e on e.code_id = mc_shares_kdj.code_id
             left join (select code_id,industry_code_id from mc_shares_join_industry) f on f.code_id = mc_shares_kdj.code_id
-            where j <16 and date_as = "%s" and mc_shares_kdj.code_id not in (SELECT code_id FROM `mc_shares_ban` )
-            and mc_shares_kdj.code_id not in (SELECT code FROM `mc_shares_name` where name like "%s" )
+            where j <16 and date_as = '%s' and mc_shares_kdj.code_id not in (SELECT code_id FROM `mc_shares_ban` )
+            and mc_shares_kdj.code_id not in (SELECT code FROM `mc_shares_name` where name like '%s' )
             and (mc_shares_kdj.code_id < 300000 or mc_shares_kdj.code_id > 600000)
             and mc_shares_kdj.code_id < 800000
             and  ((k - j) > 0 and (d - j) > 0) and ((k - j) <=5 and (d - j) <= 5)
             and c.p_end > d.p_end
             and f.industry_code_id in (
                 select mc_shares_industry.code_id from mc_shares_industry
-                left join (select code_id,p_end from mc_shares_industry where date_as = "%s") z on z.code_id = mc_shares_industry.code_id
-                where date_as = "%s" and z.p_end < mc_shares_industry.p_end
+                left join (select code_id,p_end from mc_shares_industry where date_as = '%s') z on z.code_id = mc_shares_industry.code_id
+                where date_as = '%s' and z.p_end < mc_shares_industry.p_end
             )
             having rate < 1
         '''
@@ -114,19 +114,19 @@ class Command(BaseCommand):
     def compute3(self, first, second):
         sql = '''
         SELECT 1 as id, mc_shares_kdj.code_id FROM `mc_shares_kdj`
-left join (select p_end,code_id from mc_shares where date_as = "%s") c on c.code_id = mc_shares_kdj.code_id
-left join (select p_end,code_id from mc_shares where date_as = "%s") d on d.code_id = mc_shares_kdj.code_id
+left join (select p_end,code_id from mc_shares where date_as = '%s') c on c.code_id = mc_shares_kdj.code_id
+left join (select p_end,code_id from mc_shares where date_as = '%s') d on d.code_id = mc_shares_kdj.code_id
 left join (select code_id,industry_code_id from mc_shares_join_industry) f on f.code_id = mc_shares_kdj.code_id
-where j <16 and date_as = "%s" and mc_shares_kdj.code_id not in (SELECT code_id FROM `mc_shares_ban` )
-and mc_shares_kdj.code_id not in (SELECT code FROM `mc_shares_name` where name like "%s" )
+where j <16 and date_as = '%s' and mc_shares_kdj.code_id not in (SELECT code_id FROM `mc_shares_ban` )
+and mc_shares_kdj.code_id not in (SELECT code FROM `mc_shares_name` where name like '%s' )
 and (mc_shares_kdj.code_id < 300000 or mc_shares_kdj.code_id > 600000)
 and mc_shares_kdj.code_id < 800000
 and  ((k - j) > 0 and (d - j) > 0) and ((k - j) <=5 and (d - j) <= 5)
 and c.p_end > d.p_end
 and f.industry_code_id in (
     select mc_shares_industry.code_id from mc_shares_industry
-    left join (select code_id,p_end from mc_shares_industry where date_as = "%s") z on z.code_id = mc_shares_industry.code_id
-    where date_as = "%s" and z.p_end < mc_shares_industry.p_end
+    left join (select code_id,p_end from mc_shares_industry where date_as = '%s') z on z.code_id = mc_shares_industry.code_id
+    where date_as = '%s' and z.p_end < mc_shares_industry.p_end
     )
         '''
         return SharesKdjCompute.objects.raw(sql, params=(second, first, second, '%ST%', first, second,))
