addf1842e4e209f552e88cf1c8c5c8dc01703a98
mazuqiang <844596330@qq.com>
2022-01-01 13:51:11 +0800

    添加板块抓取

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 export PATH=$PATH:/usr/local/bin
 echo -n "开始执行爬虫"
 source /alidata/python/venv/bin/activate
+cd /alidata/python/python-scrapy-article-master && nohup python main3.py
+cd /alidata/python/python-scrapy-article-master && nohup python main5.py
 cd /alidata/python/python-scrapy-article-master && nohup python main.py
 cd /alidata/python/python-scrapy-article-master && nohup python shares-zl.py
 cd /alidata/python/python-scrapy-article-master && nohup python shares.py
