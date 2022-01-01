51236c208982b7c44a3c115aa38e80116946a23d
mazuqiang <844596330@qq.com>
2022-01-01 15:28:15 +0800

    添加板块抓取

--- weixin/spiders/shares_industry_date.py
+++ weixin/spiders/shares_industry_date.py
@@ -49,6 +49,7 @@ class Shares_industry_date(scrapy.Spider):
 
     # http://11.push2.eastmoney.com/api/qt/clist/get?cb=&pn=1&pz=20&po=1&np=1&ut=bd1d9ddb04089700cf9c27f6f7426281&fltt=2&invt=2&fid=f3&fs=m:1+t:2,m:1+t:23&fields=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152&_=1584074266443"
     def start_requests(self):
+        self.connect()
         results = self.findIndustry()
         print(results)
 
