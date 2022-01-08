a577fbeee251582e5398d4c1f381977fba1c11c8
mazuqiang <844596330@qq.com>
2022-01-08 11:19:20 +0800

    添加板块抓取

--- stock/shares/management/commands/kdj3.py
+++ stock/shares/management/commands/kdj3.py
@@ -74,11 +74,11 @@ class Command(BaseCommand):
             left join (select p_end,code_id from mc_shares where date_as = %s) d on d.code_id = mc_shares_kdj.code_id
             left join (select p_end,code_id,p_max from mc_shares where date_as = %s) e on e.code_id = mc_shares_kdj.code_id
             left join (select code_id,industry_code_id from mc_shares_join_industry) f on f.code_id = mc_shares_kdj.code_id
-            where j <20 and date_as = %s and mc_shares_kdj.code_id not in (SELECT code_id FROM `mc_shares_ban` )
+            where j <16 and date_as = %s and mc_shares_kdj.code_id not in (SELECT code_id FROM `mc_shares_ban` )
             and mc_shares_kdj.code_id not in (SELECT code FROM `mc_shares_name` where name like %s )
             and (mc_shares_kdj.code_id < 300000 or mc_shares_kdj.code_id > 600000)
             and mc_shares_kdj.code_id < 800000
-            and  ((k - j) < 0 and (d - j) < 0) and ((k - j) <=5 and (d - j) <= 5)
+            and  ((k - j) > 0 and (d - j) > 0) and ((k - j) <=5 and (d - j) <= 5)
             and c.p_end > d.p_end
             and f.industry_code_id in (
                 select mc_shares_industry.code_id from mc_shares_industry
@@ -96,11 +96,11 @@ class Command(BaseCommand):
             left join (select p_end,code_id from mc_shares where date_as = %s) d on d.code_id = mc_shares_kdj.code_id
             left join (select p_end,code_id,p_max from mc_shares where date_as = %s) e on e.code_id = mc_shares_kdj.code_id
             left join (select code_id,industry_code_id from mc_shares_join_industry) f on f.code_id = mc_shares_kdj.code_id
-            where j <20 and date_as = %s and mc_shares_kdj.code_id not in (SELECT code_id FROM `mc_shares_ban` )
+            where j <16 and date_as = %s and mc_shares_kdj.code_id not in (SELECT code_id FROM `mc_shares_ban` )
             and mc_shares_kdj.code_id not in (SELECT code FROM `mc_shares_name` where name like %s )
             and (mc_shares_kdj.code_id < 300000 or mc_shares_kdj.code_id > 600000)
             and mc_shares_kdj.code_id < 800000
-            and  ((k - j) < 0 and (d - j) < 0) and ((k - j) <=5 and (d - j) <= 5)
+            and  ((k - j) > 0 and (d - j) > 0) and ((k - j) <=5 and (d - j) <= 5)
             and c.p_end > d.p_end
             and f.industry_code_id in (
                 select mc_shares_industry.code_id from mc_shares_industry
@@ -117,11 +117,11 @@ class Command(BaseCommand):
 left join (select p_end,code_id from mc_shares where date_as = %s) c on c.code_id = mc_shares_kdj.code_id
 left join (select p_end,code_id from mc_shares where date_as = %s) d on d.code_id = mc_shares_kdj.code_id
 left join (select code_id,industry_code_id from mc_shares_join_industry) f on f.code_id = mc_shares_kdj.code_id
-where j <20 and date_as = %s and mc_shares_kdj.code_id not in (SELECT code_id FROM `mc_shares_ban` )
+where j <16 and date_as = %s and mc_shares_kdj.code_id not in (SELECT code_id FROM `mc_shares_ban` )
 and mc_shares_kdj.code_id not in (SELECT code FROM `mc_shares_name` where name like %s )
 and (mc_shares_kdj.code_id < 300000 or mc_shares_kdj.code_id > 600000)
 and mc_shares_kdj.code_id < 800000
-and  ((k - j) < 0 and (d - j) < 0) and ((k - j) <=5 and (d - j) <= 5)
+and  ((k - j) > 0 and (d - j) > 0) and ((k - j) <=5 and (d - j) <= 5)
 and c.p_end > d.p_end
 and f.industry_code_id in (
     select mc_shares_industry.code_id from mc_shares_industry
