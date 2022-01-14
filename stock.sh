af5cb72f1a0969e424c4b0bbfa619267a043f5fa
mazuqiang <844596330@qq.com>
2022-01-14 20:09:04 +0800

    计算行业kdj

--- stock.sh
+++ stock.sh
@@ -2,4 +2,4 @@
 export PATH=$PATH:/usr/local/bin
 echo -n "开始执行爬虫"
 source /alidata/python/venv/bin/activate
-cd /alidata/python/python-scrapy-article-master/stock && python manage.py  closepoll
+cd /alidata/python/python-scrapy-article-master/stock && python manage.py  closepoll && python manage.py  industry
