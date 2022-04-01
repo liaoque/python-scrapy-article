faba25694df92b19b9049954db13b5708078ae27
mazuqiang <844596330@qq.com>
2022-04-01 15:58:14 +0800

    统计每个股票的季度涨幅概率

--- stock/shares/management/commands/shares_half_year.py
+++ stock/shares/management/commands/shares_half_year.py
@@ -1,13 +1,13 @@
 import numpy as np
 from datetime import datetime, date
 from django.core.management.base import BaseCommand, CommandError
-from django.db.models import Count,Max,Min
+from django.db.models import Count, Max, Min
 from django.db import connection
-
 
 # from ....polls.models import Question as Poll
 from shares.model.shares_name import SharesName
 from shares.model.shares import Shares
+from shares.model.shares_half_year import SharesHalfYear
 import time
 
 
@@ -31,26 +31,42 @@ class Command(BaseCommand):
             p_year_end = sharesItemEnd.date_as.strftime('%Y')
             print(p_year_end)
 
-
             while p_year <= p_year_end:
                 date_start = p_year + "-01-01"
                 date_end = p_year + "-06-30"
                 for item in Shares.objects.filter(code_id=code,
                                                   date_as__gte=date_start,
                                                   date_as__lte=date_end
-                                                  ).aggregate(p_start1=Min('p_start'),  p_end1=Max('p_start')):
-                    print(item)
-                print(connection.queries)
+                                                  ).aggregate(p_start1=Min('p_start'), p_end1=Max('p_start')):
+
+                    if SharesHalfYear.objects.filter(code_id=code, p_year=int(p_year), p_year_half=1).count():
+                        continue
+
+
+                    halfYear.SharesHalfYear(code_id=code, p_start=item.p_start1, p_end=item.p_end1, p_year=int(p_year),
+                                            p_year_half=1)
+                    halfYear.save()
+                # print(connection.queries)
+
+
+
                 date_start = p_year + "-07-01"
                 date_end = p_year + "-12-31"
                 for item in Shares.objects.filter(code_id=code,
                                                   date_as__gte=date_start,
                                                   date_as__lte=date_end
-                                                  ).aggregate(p_start1=Min('p_start'),  p_end1=Max('p_start')):
-                    print(item)
+                                                  ).aggregate(p_start1=Min('p_start'), p_end1=Max('p_start')):
+
+                    if SharesHalfYear.objects.filter(code_id=code, p_year=int(p_year), p_year_half=1).count():
+                        continue
+
+                    halfYear = SharesHalfYear(code_id=code, p_start=item.p_start1, p_end=item.p_end1,
+                                              p_year=int(p_year),
+                                              p_year_half=2)
+                    halfYear.save()
+
                 p_year = str(int(p_year) + 1)
                 # sql = '''
                 #     select min() from mc_shares_kdj where date_as >= '2021-12-01' group by date_as;
                 #     '''
                 # return SharesKdjCompute.objects.raw(sql)
-
