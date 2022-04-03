a5707db42e8b8fb03c8de54802c94e693e189b4d
mazuqiang <844596330@qq.com>
2022-04-04 00:31:27 +0800

    统计每个行业的月度涨幅概率

--- stock/shares/management/commands/shares_industry_month.py
+++ stock/shares/management/commands/shares_industry_month.py
@@ -7,6 +7,7 @@ from django.db import connection
 # from ....polls.models import Question as Poll
 from shares.model.shares_name import SharesName
 from shares.model.shares import Shares
+from shares.model.shares_industry import SharesIndustry
 from shares.model.shares_industry_month import SharesIndustryMonth
 import time
 
@@ -22,10 +23,10 @@ class Command(BaseCommand):
     help = '统计每月的 行业 最高和最低'
 
     def handle(self, *args, **options):
-        for item in SharesName.objects.filter(status=1, code_type=1):
+        for item in SharesName.objects.filter(status=1, code_type=2):
             code = item.code
-            sharesItem = Shares.objects.filter(code_id=code).order_by('date_as')[0]
-            sharesItemEnd = Shares.objects.filter(code_id=code).order_by('-date_as')[0]
+            sharesItem = SharesIndustry.objects.filter(code_id=code).order_by('date_as')[0]
+            sharesItemEnd = SharesIndustry.objects.filter(code_id=code).order_by('-date_as')[0]
             p_year = sharesItem.date_as.strftime('%Y')
             p_year_end = sharesItemEnd.date_as.strftime('%Y')
             print(p_year_end)
@@ -49,14 +50,14 @@ class Command(BaseCommand):
                 p_year = str(int(p_year) + 1)
 
     def saveMonth(self, code, p_year, date_start, date_end, p_month):
-        halfYearSharesStart = Shares.objects.filter(code_id=code,
+        halfYearSharesStart = SharesIndustry.objects.filter(code_id=code,
                                                     date_as__gte=date_start,
                                                     date_as__lte=date_end
                                                     )
         if len(halfYearSharesStart) == 0:
             return
 
-        halfYearSharesEnd = Shares.objects.filter(code_id=code,
+        halfYearSharesEnd = SharesIndustry.objects.filter(code_id=code,
                                                   date_as__lte=date_end
                                                   ).order_by('-date_as')[0]
 
