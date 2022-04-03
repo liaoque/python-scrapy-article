6b0133a9f680e4723780380a5ebde42cb0410560
mazuqiang <844596330@qq.com>
2022-04-03 11:33:23 +0800

    统计每个股票的月度涨幅概率

--- stock/shares/management/commands/shares_half_statistics.py
+++ stock/shares/management/commands/shares_half_statistics.py
@@ -62,3 +62,20 @@ class Command(BaseCommand):
             print("\n%s季度---start\n" % season)
             print(",".join(["\"" + item.code_id + "\"" for item in slist]))
             print("\n%s季度---end\n" % season)
+
+    def month(self):
+        for season in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]:
+            sql = """
+                    SELECT 1 as id, code_id FROM (SELECT t1.code_id,  t1.c / t2.c as rang 
+            from (SELECT COUNT(1) as c, code_id FROM `mc_shares_month` where  p_month = %s and p_end > p_start GROUP BY code_id) t1
+            LEFT JOIN 
+                (SELECT COUNT(1) as c, code_id FROM `mc_shares_month` where  p_month = %s GROUP BY code_id) t2 
+            on t1.code_id = t2.code_id
+            where t1.code_id not in (SELECT code from mc_shares_name where name like "%s"  )
+            HAVING rang > 0.7 and rang < 1 and t1.code_id < '700000') c;
+                    """
+            print(sql)
+            slist = SharesHalfYear.objects.raw(sql, params=(season, season, "ST%"))
+            print("\n%s季度---start\n" % season)
+            print(",".join(["\"" + item.code_id + "\"" for item in slist]))
+            print("\n%s季度---end\n" % season)
