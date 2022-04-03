b79650dcdc39397527c34d1ef3586fb86221c26c
mazuqiang <844596330@qq.com>
2022-04-03 11:12:38 +0800

    统计每个股票的月度涨幅概率

--- stock/shares/management/commands/shares_half_statistics.py
+++ stock/shares/management/commands/shares_half_statistics.py
@@ -27,7 +27,7 @@ class Command(BaseCommand):
 
     def half_year(self):
         sql = """
-                SELECT code_id FROM (SELECT t1.code_id,  t1.c / t2.c as rang 
+                SELECT 1 as id, code_id FROM (SELECT t1.code_id,  t1.c / t2.c as rang 
         from (SELECT COUNT(1) as c, code_id FROM `mc_shares_half_year` where  p_year_half = 1 and p_end > p_start GROUP BY code_id) t1
         LEFT JOIN 
             (SELECT COUNT(1) as c, code_id FROM `mc_shares_half_year` where  p_year_half = 1 GROUP BY code_id) t2 
@@ -43,16 +43,16 @@ class Command(BaseCommand):
         print(",".join(["\"" + item + "\"" for item in slist]))
         print("上半年---end")
         sql = """
-                        SELECT code_id FROM (SELECT t1.code_id,  t1.c / t2.c as rang 
-                from (SELECT COUNT(1) as c, code_id FROM `mc_shares_half_year` where  p_year_half = 2 and p_end > p_start GROUP BY code_id) t1
-                LEFT JOIN 
-                    (SELECT COUNT(1) as c, code_id FROM `mc_shares_half_year` where  p_year_half = 2 GROUP BY code_id) t2 
-                on t1.code_id = t2.code_id
-                where t1.code_id not in (SELECT code from mc_shares_name where name like "%s" )
-                HAVING rang > 0.7 and rang < 1) c;
-                        """ 
+                SELECT 1 as id, code_id FROM (SELECT t1.code_id,  t1.c / t2.c as rang 
+        from (SELECT COUNT(1) as c, code_id FROM `mc_shares_half_year` where  p_year_half = 2 and p_end > p_start GROUP BY code_id) t1
+        LEFT JOIN 
+            (SELECT COUNT(1) as c, code_id FROM `mc_shares_half_year` where  p_year_half = 2 GROUP BY code_id) t2 
+        on t1.code_id = t2.code_id
+        where t1.code_id not in (SELECT code from mc_shares_name where name like "%s" )
+        HAVING rang > 0.7 and rang < 1) c;
+                """
         print(sql)
-        slist = SharesHalfYear.objects.raw(sql, params=("ST%", ))
+        slist = SharesHalfYear.objects.raw(sql, params=("ST%",))
         print(slist)
         print("\n下半年---start\n")
         print(",".join(["\"" + item + "\"" for item in slist]))
@@ -61,16 +61,16 @@ class Command(BaseCommand):
     def season(self):
         for season in [1, 2, 3, 4]:
             sql = """
-                            SELECT code_id FROM (SELECT t1.code_id,  t1.c / t2.c as rang 
-                    from (SELECT COUNT(1) as c, code_id FROM `mc_shares_season` where  p_season = %s and p_end > p_start GROUP BY code_id) t1
-                    LEFT JOIN 
-                        (SELECT COUNT(1) as c, code_id FROM `mc_shares_season` where  p_season = %s GROUP BY code_id) t2 
-                    on t1.code_id = t2.code_id
-                    where t1.code_id not in (SELECT code from mc_shares_name where name like "%s"  )
-                    HAVING rang > 0.7 and rang < 1) c;
-                            """ % season, season, "ST%"
+                    SELECT 1 as id, code_id FROM (SELECT t1.code_id,  t1.c / t2.c as rang 
+            from (SELECT COUNT(1) as c, code_id FROM `mc_shares_season` where  p_season = %s and p_end > p_start GROUP BY code_id) t1
+            LEFT JOIN 
+                (SELECT COUNT(1) as c, code_id FROM `mc_shares_season` where  p_season = %s GROUP BY code_id) t2 
+            on t1.code_id = t2.code_id
+            where t1.code_id not in (SELECT code from mc_shares_name where name like "%s"  )
+            HAVING rang > 0.7 and rang < 1) c;
+                    """
             print(sql)
-            slist = SharesHalfYear.objects.raw(sql, params=())
+            slist = SharesHalfYear.objects.raw(sql, params=(season, season, "ST%"))
             print("\n%s季度---start\n" % season)
             print(",".join(["\"" + item + "\"" for item in slist]))
             print("\n%s季度---end\n" % season)
