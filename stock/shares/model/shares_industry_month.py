331e7c310a9ed525fd4e2ead125ba1e3013595c2
mazuqiang <844596330@qq.com>
2022-04-03 12:51:23 +0800

    统计每个行业的月度涨幅概率

---
+++ stock/shares/model/shares_industry_month.py
@@ -0,0 +1,22 @@
+from django.db import models
+from .shares_name import SharesName
+from datetime import datetime
+
+
+# Create your models here.
+
+
+class SharesIndustryMonth(models.Model):
+    code = models.ForeignKey(SharesName, on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    p_start = models.IntegerField(default=0)
+    p_end = models.IntegerField(default=0)
+    p_year = models.IntegerField(default=0)
+    p_month = models.IntegerField(default=0, help_text="1 第一季，2 第二季")
+
+    class Meta:
+        db_table = "mc_shares_industry_month"
+        # abstract = True
+
+    def __str__(self):
+        return self.name + ":" + self.p_year + ":" + self.p_month + ":最大-" + self.p_start + ":最小-" + self.p_end
