47de86db86ccf310f812f794cca3233de5e6ae46
mazuqiang <844596330@qq.com>
2022-04-23 23:32:08 +0800

    kdj监控

--- stock/shares/management/commands/kdj-listen.py
+++ stock/shares/management/commands/kdj-listen.py
@@ -49,13 +49,14 @@ class Command(BaseCommand):
             print("寻找卖出点-----")
             for codeItem in allCodeIds:
                 codeItemResult = self.findSellPoint(codeItem)
+                sharesItem = Shares.objects.filter(date_as=codeItemResult.date_as, code_id=codeItem.code_id)[0]
                 print("找到卖出点--%s---%s", codeItemResult.date_as, sharesItem.p_end)
                 buys = SharesBuys(
                     code_id=codeItem.code_id,
                     buy_date_as=codeItem.buy_date_as,
                     buy_start=codeItem.buy_start,
                     sell_date_as=codeItemResult.date_as,
-                    sell_end=codeItemResult.p_end,
+                    sell_end=sharesItem.p_end,
                 )
                 buys.save()
                 codeItem.delete()
