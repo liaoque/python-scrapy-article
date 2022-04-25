f66ff5543e75189cc0685dd0fbcf80c8a4484191
mazuqiang <844596330@qq.com>
2022-04-25 11:33:21 +0800

    kdj监控

--- stock/shares/management/commands/kdj-listen.py
+++ stock/shares/management/commands/kdj-listen.py
@@ -108,7 +108,7 @@ class Command(BaseCommand):
                     '-date_as')[:14]
                 shares = shares[::-1]
                 close = [item.p_end / 100 for item in shares]
-                pre_end =shares[len(shares) -1].p_end / 100*1.003
+                pre_end =shares[len(shares) -1].p_end / 100
                 close.append(pre_end)
                 emaList = talib.EMA(np.array(close), timeperiod=7)
                 print("%s--%s--%s--%s", sharesKdjItem.date_as, codeItem.code_id, emaList[len(emaList) - 1] , emaList[len(emaList) - 2])
