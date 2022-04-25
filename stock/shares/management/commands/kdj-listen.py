71b045dec41175bafe329c470eac7f3828bd04ce
mazuqiang <844596330@qq.com>
2022-04-25 10:53:05 +0800

    kdj监控

--- stock/shares/management/commands/kdj-listen.py
+++ stock/shares/management/commands/kdj-listen.py
@@ -109,7 +109,8 @@ class Command(BaseCommand):
                 shares = shares[::-1]
                 close = [item.p_end / 100 for item in shares]
                 print(close, type(close))
-                close.push(shares[len(shares) -1] * 0.96)
+                close.append(shares[len(shares) -1] * 0.96)
+                print(close, type(close))
                 emaList = talib.EMA(np.array(close), timeperiod=7)
                 print(emaList, emaList[len(emaList) - 1] , emaList[len(emaList) - 2])
                 if emaList[len(emaList) - 1] > emaList[len(emaList) - 2]:
