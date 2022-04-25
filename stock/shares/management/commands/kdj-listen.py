e809fedd267427a0bb27cee73c55329c84f8742f
mazuqiang <844596330@qq.com>
2022-04-25 14:00:54 +0800

    kdj监控

--- stock/shares/management/commands/kdj-listen.py
+++ stock/shares/management/commands/kdj-listen.py
@@ -62,12 +62,16 @@ class Command(BaseCommand):
                         sell_end=sharesItem.p_end,
                     )
                     buys.save()
-                    if codeItem.buy_start < sharesItem.p_end:
-                        codeItem.delete()
-                    else:
-                        codeItem.date_as = codeItemResult.date_as
-                        codeItem.buy_start = 0
-                        codeItem.save()
+                    codeItem.delete()
+                    if codeItem.buy_start >= sharesItem.p_end:
+                        listen = SharesDateListen(
+                            code_id=codeItem.code_id,
+                            p_start=codeItem.buy_start,
+                            date_as=codeItemResult.date_as,
+                            type=1,
+                        )
+                        listen.save()
+
                 # break
 
     def findSellPoint(self, codeItem):
@@ -127,8 +131,8 @@ class Command(BaseCommand):
                     preEma = ((emaList[-1] + .01) * (7 + 1) - (7 - 1) * emaList[-1]) / 2
                     sharesItem = Shares.objects.filter(date_as__lte=result[key + 2].date_as, code_id=codeItem.code_id)[
                         0]
-                    print(sharesKdjItem.date_as, codeItem.code_id,sharesItem.p_end, preEma, emaList[-1] )
-                    if sharesItem and (sharesItem.p_end - sharesItem.p_start)/2 * sharesItem.p_start > preEma:
+                    print(sharesKdjItem.date_as, codeItem.code_id, sharesItem.p_end, preEma, emaList[-1])
+                    if sharesItem and (sharesItem.p_end - sharesItem.p_start) / 2 * sharesItem.p_start > preEma:
                         item = result[key + 1]
                 break
 
