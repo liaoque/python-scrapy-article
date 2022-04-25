cfe77691530cd16a58a4339d3d4316694a0e69a6
mazuqiang <844596330@qq.com>
2022-04-25 21:46:20 +0800

    kdj监控

--- stock/shares/management/commands/kdj-listen.py
+++ stock/shares/management/commands/kdj-listen.py
@@ -127,19 +127,19 @@ class Command(BaseCommand):
                 #     print(sharesKdjItem.date_as, codeItem.code_id, [item.p_end for item in shares[1:]], sharesSum,
                 #       shares[0].p_end)
                 # if sharesSum / 5 > shares[0].p_end:
-                    shares = Shares.objects.filter(date_as__lte=sharesKdjItem.date_as, code_id=codeItem.code_id)
-                    close = [item.p_end / 100 for item in shares]
-                    emaList = talib.EMA(np.array(close), timeperiod=5)
-                    # print(emaList)
-                    # preEma = (2 * x + (5 - 1) * emaList[-1]) / (5 + 1)
-                    preEma = ((emaList[-1] + .01) * (5 + 1) - (5 - 1) * emaList[-1]) / 2
-                    sharesItem = Shares.objects.filter(date_as__lte=result[key + 2].date_as, code_id=codeItem.code_id)[
-                        0]
-                    if codeItem.code_id == '001317':
-                        print(sharesKdjItem.date_as, codeItem.code_id, sharesItem.p_end, preEma, emaList[-1])
-                    if sharesItem and (sharesItem.p_end - sharesItem.p_start) / 2 * sharesItem.p_start > preEma:
-                        item = result[key + 1]
-                        break
+                shares = Shares.objects.filter(date_as__lte=sharesKdjItem.date_as, code_id=codeItem.code_id)
+                close = [item.p_end / 100 for item in shares]
+                emaList = talib.EMA(np.array(close), timeperiod=5)
+                # print(emaList)
+                # preEma = (2 * x + (5 - 1) * emaList[-1]) / (5 + 1)
+                preEma = ((emaList[-1] + .01) * (5 + 1) - (5 - 1) * emaList[-1]) / 2
+                sharesItem = Shares.objects.filter(date_as__lte=result[key + 2].date_as, code_id=codeItem.code_id)[
+                    0]
+                if codeItem.code_id == '001317':
+                    print(sharesKdjItem.date_as, codeItem.code_id, sharesItem.p_end, preEma, emaList[-1])
+                if sharesItem and (sharesItem.p_end - sharesItem.p_start) / 2 * sharesItem.p_start > preEma:
+                    item = result[key + 1]
+                    break
             key += 1
         return item
 
