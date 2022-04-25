e393598fb0fd83b0b438b6096bebc1fee6d769c9
mazuqiang <844596330@qq.com>
2022-04-25 23:38:51 +0800

    kdj监控

--- stock/shares/model/shares_buys.py
+++ stock/shares/model/shares_buys.py
@@ -9,8 +9,10 @@ class SharesBuys(models.Model):
     code = models.ForeignKey(SharesName, name='code', on_delete=models.CASCADE)
     buy_date_as = models.DateField(null=True)
     buy_start = models.IntegerField(default=0)
+    buy_pre = models.IntegerField(default=0)
     sell_date_as = models.DateField(null=True)
     sell_end = models.IntegerField(default=0)
+    sell_pre = models.IntegerField(default=0)
 
 
     class Meta:
