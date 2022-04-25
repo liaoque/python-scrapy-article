8792632fd70ea100e7ca43bfecd225054575a64f
mazuqiang <844596330@qq.com>
2022-04-25 22:41:59 +0800

    kdj监控

--- stock/shares/management/commands/kdj-listen.py
+++ stock/shares/management/commands/kdj-listen.py
@@ -130,7 +130,7 @@ class Command(BaseCommand):
                 # if sharesSum / 5 > shares[0].p_end:
                 shares = Shares.objects.filter(date_as__lte=sharesKdjItem.date_as, code_id=codeItem.code_id)
                 close = [item.p_end / 100 for item in shares]
-                emaList7 = talib.EMA(np.array(close), timeperiod=6)
+                emaList7 = talib.EMA(np.array(close), timeperiod=8)
                 emaList4 = talib.EMA(np.array(close), timeperiod=4)
                 print(sharesKdjItem.date_as, codeItem.code_id,emaList4[-2] , emaList7[-2] , emaList4[-1] , emaList7[-1])
                 if not (emaList4[-2] < emaList7[-2] and emaList4[-1] > emaList7[-1]):
