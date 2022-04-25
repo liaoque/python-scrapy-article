5f436bb4787d9d28acb6b08fa54bfeebd52d5fd5
mazuqiang <844596330@qq.com>
2022-04-25 13:33:14 +0800

    kdj监控

--- stock/shares/management/commands/kdj-listen.py
+++ stock/shares/management/commands/kdj-listen.py
@@ -39,6 +39,9 @@ class Command(BaseCommand):
                 for codeItem in allCodeIds:
                     codeItemResult = self.findBuyPoint(codeItem)
                     if codeItemResult != None:
+                        if len(SharesDateListen.objects.filter(code_id=codeItem.code_id)):
+                            continue
+
                         sharesItem = Shares.objects.filter(date_as=codeItemResult.date_as, code_id=codeItem.code_id)[0]
                         print("找到买入点--%s---%s", codeItemResult.date_as, sharesItem.p_end)
                         codeItem.buy_date_as = codeItemResult.date_as
