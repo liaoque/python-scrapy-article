e6aafcc8c398bdd17c5294016abd7bb30b03a7b0
mazuqiang <844596330@qq.com>
2022-05-10 18:56:26 +0800

    重写

--- stock/shares/management/commands/shares_wheel.py
+++ stock/shares/management/commands/shares_wheel.py
@@ -52,6 +52,7 @@ class Command(BaseCommand):
             where date_as >= %s and date_as <= %s  and  industry_code_id = %s
                """
             result2 = Shares.objects.raw(sql, params=(start, end, item.industry_code_id,))
+            item.sc = result2[0].c
             print(item.industry_name, item.industry_code_id, item.c, result2[0].c, item.c / result2[0].c)
 
     def getAllDates(self):
