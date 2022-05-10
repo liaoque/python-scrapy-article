f7f29c7d3280d50988a85d9c6864d89ae1602d5b
mazuqiang <844596330@qq.com>
2022-05-10 18:46:44 +0800

    重写

--- stock/shares/management/commands/shares_wheel.py
+++ stock/shares/management/commands/shares_wheel.py
@@ -46,12 +46,12 @@ class Command(BaseCommand):
         for item in result:
             if item.industry_code_id is None:
                 continue
-            """
+            sql = """
             select 1 as id, count(1) as c from mc_shares 
             left join mc_shares_join_industry on mc_shares_join_industry.code_id = mc_shares.code_id
             where date_as >= %s and date_as <= %s  and  industry_code_id = %s
                """
-            result2 = Shares.objects.raw(sql, params=(start, end, str(item.industry_code_id),))
+            result2 = Shares.objects.raw(sql, params=(start, end, item.industry_code_id,))
             print(result2)
             print(item.industry_name, item.industry_code_id, item.c, result2[0].c, item.c / result2[0].c)
 
