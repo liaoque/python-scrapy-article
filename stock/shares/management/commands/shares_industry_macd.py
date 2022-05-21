224acfeaa4fe1f3e96ee54387dc2e80e0a953d21
mazuqiang <844596330@qq.com>
2022-05-21 18:41:37 +0800

    趋势统计

--- stock/shares/management/commands/shares_industry_macd.py
+++ stock/shares/management/commands/shares_industry_macd.py
@@ -80,7 +80,7 @@ class Command(BaseCommand):
         # 计算kd指标
         close_prices = [int(v['p_end']) / 100 for v in data]
         if len(close_prices) < 33:
-            close_prices = [close_prices[0]] * 32 + close_prices
+            close_prices = [close_prices[0]] * 31 + close_prices
         close_prices = np.array(close_prices)
         macdDIFF, macdDEA, macd = talib.MACDEXT(close_prices, fastperiod=12, fastmatype=1, slowperiod=26, slowmatype=1,
                                                 signalperiod=9, signalmatype=1)
