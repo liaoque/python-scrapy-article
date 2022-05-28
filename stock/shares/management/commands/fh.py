918a6011e8d7d6d5b3df3ef706a82fc8e13bbdc3
mazuqiang <844596330@qq.com>
2022-05-28 23:08:20 +0800

    板块记录

--- stock/shares/management/commands/fh.py
+++ stock/shares/management/commands/fh.py
@@ -57,29 +57,36 @@ class Command(BaseCommand):
                 print(item)
                 if item["NOTICE_DATE"] is not None:
                     time1 = datetime.strptime(item["NOTICE_DATE"], '%Y-%m-%d 00:00:00').date()
-                    for item in Shares.objects.filter(date_as=time1, code_id=code):
-                        print(item)
-                        if item.p_start < item.p_end:
-                            all["notice"]["low"] += 1
-                        else:
-                            all["notice"]["low"] += 0
-                    pass
+                    itemAll = Shares.objects.filter(date_as=time1, code_id=code)
+                    if len(itemAll) > 0:
+                        for item in itemAll:
+                            print(item)
+                            if item.p_start < item.p_end:
+                                all["notice"]["low"] += 1
+                            else:
+                                all["notice"]["low"] += 0
+
                 if item["EQUITY_RECORD_DATE"] is not None:
                     time1 = datetime.strptime(item["EQUITY_RECORD_DATE"], '%Y-%m-%d 00:00:00').date()
-                    for item in Shares.objects.filter(date_as=time1, code_id=code):
-                        if item.p_start < item.p_end:
-                            all["equity"]["low"] += 1
-                        else:
-                            all["equity"]["low"] += 0
-                    pass
+                    itemAll = Shares.objects.filter(date_as=time1, code_id=code)
+                    if len(itemAll) > 0:
+                        for item in itemAll:
+                            if item.p_start < item.p_end:
+                                all["equity"]["low"] += 1
+                            else:
+                                all["equity"]["low"] += 0
+                        pass
+
                 if item["EX_DIVIDEND_DATE"] is not None:
                     time1 = datetime.strptime(item["EX_DIVIDEND_DATE"], '%Y-%m-%d 00:00:00').date()
-                    for item in Shares.objects.filter(date_as=time1, code_id=code):
-                        if item.p_start < item.p_end:
-                            all["ex"]["low"] += 1
-                        else:
-                            all["ex"]["low"] += 0
-                    pass
+                    itemAll = Shares.objects.filter(date_as=time1, code_id=code)
+                    if len(itemAll) > 0:
+                        for item in itemAll:
+                            if item.p_start < item.p_end:
+                                all["ex"]["low"] += 1
+                            else:
+                                all["ex"]["low"] += 0
+                        pass
                 pass
 
         print(all)
