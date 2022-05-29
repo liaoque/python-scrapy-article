6071c330fac656f646892d103abd07aed58bdd65
mazuqiang <844596330@qq.com>
2022-05-30 00:35:14 +0800

    fh

--- stock/shares/management/commands/fh.py
+++ stock/shares/management/commands/fh.py
@@ -30,8 +30,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         today = datetime.now().date().strftime('%Y-%m-%d')
-        # self.calculate(today)
-        self.calculate("2022-05-30")
+        self.calculate(today)
+        # self.calculate("2022-05-30")
         pass
 
     def calculateCheck(self, today):
