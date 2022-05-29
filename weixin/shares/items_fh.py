d3c1f7773c9a5fb2e4b2be63b1650434cc42b137
mazuqiang <844596330@qq.com>
2022-05-29 12:57:45 +0800

    fh

--- weixin/shares/items_fh.py
+++ weixin/shares/items_fh.py
@@ -26,17 +26,16 @@ class Items(scrapy.Item):
 
     def save(self, cursor):
         code = self['code'][0]
-        date_as = self['directors_date_as'][0]
-        if self.exitsByCode(cursor, code, date_as):
+        title = self['title'][0]
+        if self.exitsByCode(cursor, code, title):
             sql = """
             update mc_shares_fh 
-                    set title = %s, info = %s, amount = %s,
+                    set info = %s, amount = %s,
                         `range` = %s, directors_date_as = %s, shareholder_date_as = %s 
                         `implement_date_as` = %s, register_date_as = %s, ex_date_as = %s 
-                    where code_id = %s and date_as=%s
+                    where code_id = %s and title=%s
             """
             params = (
-                self["title"][0],
                 self["info"][0],
                 self["amount"][0],
                 self["range"][0],
@@ -46,7 +45,7 @@ class Items(scrapy.Item):
                 self["register_date_as"][0],
                 self["ex_date_as"][0],
                 code,
-                date_as,
+                title,
             )
             cursor.execute(sql, params)
             return
@@ -54,7 +53,7 @@ class Items(scrapy.Item):
             INSERT INTO mc_shares_fh (code_id, title, info, amount, `range`, 
             directors_date_as, shareholder_date_as,
              implement_date_as, register_date_as, ex_date_as)
-            VALUES (%s, %s, %s,  %s, %s)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
             """;
         params = (
             code,
@@ -73,10 +72,10 @@ class Items(scrapy.Item):
 
     pass
 
-    def exitsByCode(self, cursor, code, date_as):
-        sql = "SELECT code_id FROM mc_shares_fh  WHERE code_id = %s and date_as=%s"
+    def exitsByCode(self, cursor, code, title):
+        sql = "SELECT code_id FROM mc_shares_fh  WHERE code_id = %s and title=%s"
         cursor.execute(sql, (
             code,
-            date_as,
+            title,
         ));
         return cursor.rowcount > 0
