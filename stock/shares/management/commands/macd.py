18996ff3fe5b7c02562acc33c48a1f14e1913ee7
mazuqiang <844596330@qq.com>
2022-06-01 02:41:10 +0800

    fh

--- stock/shares/management/commands/macd.py
+++ stock/shares/management/commands/macd.py
@@ -26,10 +26,27 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         today = datetime.now().date().strftime('%Y-%m-%d')
-        self.calculateKdj(today)
+        # self.calculateKdj(today)
         # print("开始计算kdj-----")
         # today = datetime.now().date().strftime('%Y-%m-%d')
         # self.KdjCompute(today)
+        dateList = self.getAllDates()
+        yesterday = dateList[-2].date_as
+        sql = """
+                SELECT 1 as id, m.code_id, m.diff,m.dea, t.diff as diff2,t.dea as dea
+                from (select * from mc_shares_macd where date_as = %s) m
+                LEFT JOIN (select * from mc_shares_macd where date_as = %s) t 
+                    ON t.code_id = m.code_id 
+                """
+        result = Shares.objects.raw(sql, params=(today, yesterday,))
+        for item in result:
+            shareName2 = SharesName.objects.filter(code=item.code_id)
+            if len(shareName2) > 0:
+                if item.diff >= item.dea2 and item.dea >= item.dea2:
+                    shareName2.macd_up = 1
+                else:
+                    shareName2.macd_up = 2
+                shareName2.update()
         pass
 
     def calculateKdj(self, today):
@@ -76,3 +93,9 @@ class Command(BaseCommand):
                                                 signalperiod=9, signalmatype=1)
         macd = macd * 2
         return (macdDIFF, macdDEA, macd)
+
+    def getAllDates(self):
+        sql = '''
+                select 1 as id, date_as from mc_shares_date where date_as >= '2021-12-01;
+                '''
+        return SharesKdjCompute.objects.raw(sql)
