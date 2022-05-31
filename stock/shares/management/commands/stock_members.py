4dd2b03f88eed4b9c66e6bd2606e23c09169ebbf
mazuqiang <844596330@qq.com>
2022-06-01 02:02:23 +0800

    fh

--- stock/shares/management/commands/stock_members.py
+++ stock/shares/management/commands/stock_members.py
@@ -27,55 +27,53 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         result = []
         # 股东数持续减少的, 并排出了 银行，证券，保险
-        sql = """
-        select * from (
-        SELECT mc_stock_members.* from mc_stock_members 
-                LEFT JOIN (SELECT code_id, date_as, avg_free_shares FROM `mc_stock_members` GROUP by code_id ORDER BY date_as desc) t 
-                ON t.code_id = mc_stock_members.code_id 
-                where t.date_as > mc_stock_members.date_as 
-                and ( mc_stock_members.code_id < '300000' or (mc_stock_members.code_id > '600000' and mc_stock_members.code_id < '700000' ) )
-                GROUP by mc_stock_members.code_id 
-                ORDER BY mc_stock_members.date_as desc
-        ) as s
-        LEFT JOIN (SELECT code_id, date_as, avg_free_shares  FROM `mc_stock_members`  GROUP by code_id ORDER BY date_as desc) t 
-            ON t.code_id = s.code_id 
-        where t.avg_free_shares > 50000
-            and t.avg_free_shares > s.avg_free_shares
-            and ( s.code_id < '300000' or (s.code_id > '600000' and s.code_id < '700000' ) )
-            and s.code_id  not in (select code_id from mc_shares_join_industry where industry_code_id in ('BK0475', 'BK0474', 'BK0473'));
-        """
-        result = Shares.objects.raw(sql)
-        print(
-            "股东数持续减少的",
-            ",".join(["\"" + item.code_id + "\"" for item in result])
-        )
-
-#        并排出了 银行，证券，保险
-        sql = """
-            select * from (
-            SELECT mc_stock_members.* from mc_stock_members 
-                    LEFT JOIN (SELECT code_id, date_as, avg_free_shares FROM `mc_stock_members` GROUP by code_id ORDER BY date_as desc) t 
-                    ON t.code_id = mc_stock_members.code_id 
-                    where t.date_as > mc_stock_members.date_as 
-                    and ( mc_stock_members.code_id < '300000' or (mc_stock_members.code_id > '600000' and mc_stock_members.code_id < '700000' ) )
-                    GROUP by mc_stock_members.code_id 
-                    ORDER BY mc_stock_members.date_as desc
-            ) as s
-            LEFT JOIN (SELECT code_id, date_as, avg_free_shares,price  FROM `mc_stock_members`  GROUP by code_id ORDER BY date_as desc) t 
-                ON t.code_id = s.code_id 
-            where t.avg_free_shares > 50000
-                and t.avg_free_shares < s.avg_free_shares
-                and t.price < s.price
-                and ( s.code_id < '300000' or (s.code_id > '600000' and s.code_id < '700000' ) )
-                and s.code_id  not in (select code_id from mc_shares_join_industry where industry_code_id in ('BK0475', 'BK0474', 'BK0473'));
-                """
-        result = Shares.objects.raw(sql)
-        print(
-            "股东人数增加，股价基本没涨的股票, 还在调整",
-            ",".join(["\"" + item.code_id + "\"" for item in result])
-        )
-
-
+        # sql = """
+        # select * from (
+        # SELECT mc_stock_members.* from mc_stock_members
+        #         LEFT JOIN (SELECT code_id, date_as, avg_free_shares FROM `mc_stock_members` GROUP by code_id ORDER BY date_as desc) t
+        #         ON t.code_id = mc_stock_members.code_id
+        #         where t.date_as > mc_stock_members.date_as
+        #         and ( mc_stock_members.code_id < '300000' or (mc_stock_members.code_id > '600000' and mc_stock_members.code_id < '700000' ) )
+        #         GROUP by mc_stock_members.code_id
+        #         ORDER BY mc_stock_members.date_as desc
+        # ) as s
+        # LEFT JOIN (SELECT code_id, date_as, avg_free_shares  FROM `mc_stock_members`  GROUP by code_id ORDER BY date_as desc) t
+        #     ON t.code_id = s.code_id
+        # where t.avg_free_shares > 50000
+        #     and t.avg_free_shares > s.avg_free_shares
+        #     and ( s.code_id < '300000' or (s.code_id > '600000' and s.code_id < '700000' ) )
+        #     and s.code_id  not in (select code_id from mc_shares_join_industry where industry_code_id in ('BK0475', 'BK0474', 'BK0473'));
+        # """
+        # result = Shares.objects.raw(sql)
+        # print(
+        #     "股东数持续减少的",
+        #     ",".join(["\"" + item.code_id + "\"" for item in result])
+        # )
+        #
+        # #        并排出了 银行，证券，保险
+        # sql = """
+        #     select * from (
+        #     SELECT mc_stock_members.* from mc_stock_members
+        #             LEFT JOIN (SELECT code_id, date_as, avg_free_shares FROM `mc_stock_members` GROUP by code_id ORDER BY date_as desc) t
+        #             ON t.code_id = mc_stock_members.code_id
+        #             where t.date_as > mc_stock_members.date_as
+        #             and ( mc_stock_members.code_id < '300000' or (mc_stock_members.code_id > '600000' and mc_stock_members.code_id < '700000' ) )
+        #             GROUP by mc_stock_members.code_id
+        #             ORDER BY mc_stock_members.date_as desc
+        #     ) as s
+        #     LEFT JOIN (SELECT code_id, date_as, avg_free_shares,price  FROM `mc_stock_members`  GROUP by code_id ORDER BY date_as desc) t
+        #         ON t.code_id = s.code_id
+        #     where t.avg_free_shares > 50000
+        #         and t.avg_free_shares < s.avg_free_shares
+        #         and t.price < s.price
+        #         and ( s.code_id < '300000' or (s.code_id > '600000' and s.code_id < '700000' ) )
+        #         and s.code_id  not in (select code_id from mc_shares_join_industry where industry_code_id in ('BK0475', 'BK0474', 'BK0473'));
+        #         """
+        # result = Shares.objects.raw(sql)
+        # print(
+        #     "股东人数增加，股价基本没涨的股票, 还在调整",
+        #     ",".join(["\"" + item.code_id + "\"" for item in result])
+        # )
 
         for item in SharesName.objects.filter(status=1, code_type=1, ):
             stockMemberList = self.getStockMember(item)
@@ -95,10 +93,29 @@ class Command(BaseCommand):
                 itemSharesMembers = SharesMembers(
                     code_id=item.code, date_as=date_as,
                     members=item2['HOLDER_TOTAL_NUM'], info=item2['HOLD_FOCUS'],
-                    price=item2['PRICE'] *100,
+                    price=item2['PRICE'] * 100,
                     avg_free_shares=item2['AVG_FREE_SHARES']
                 )
                 itemSharesMembers.save()
+
+        sql = """
+        SELECT mc_stock_members.code_id,mc_stock_members.members,t.members as tmembers from mc_stock_members 
+                LEFT JOIN (SELECT code_id, date_as, avg_free_shares,members FROM `mc_stock_members` GROUP by code_id ORDER BY date_as desc) t 
+                ON t.code_id = mc_stock_members.code_id 
+                where t.date_as > mc_stock_members.date_as
+                GROUP by mc_stock_members.code_id 
+                ORDER BY mc_stock_members.code_id asc;
+        """
+        result = Shares.objects.raw(sql)
+        for item in result:
+            shareName = SharesName.objects.filter(code=item.code_id)
+            if len(shareName) > 0:
+                if item.members >= item.tmembers:
+                    shareName(code=item.code_id, member_up=2)
+                else:
+                    shareName(code=item.code_id, member_up=1)
+
+
         #
         #
         #     if len(stockMemberList) <= 1:
