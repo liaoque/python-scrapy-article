30f963cfbdb614ecf193387f232ca5853f7ea2d4
mazuqiang <844596330@qq.com>
2022-06-02 16:26:49 +0800

    fh

--- stock/shares/management/commands/kdj-listen.py
+++ stock/shares/management/commands/kdj-listen.py
@@ -269,8 +269,8 @@ left join mc_shares_join_industry as i on n.code = i.code_id
 left JOIN (SELECT * FROM `mc_shares_name` where code_type =  2 and gpm_ex > 1000) t on t.code = i.industry_code_id
 where ( n.gpm_ex > t.gpm_ex or  n.npmos_ex > t.npmos_ex)  and n.name not like "%ST%"  and n.npmos > 0 and n.member_up =1 
         """
-        codeList = SharesName.objects.raw(sql).all()
-
+        codeList = SharesName.objects.raw(sql)
+        print(codeList)
         #  公司毛利率不能低于行业毛利率的 30%
         codeList = filter(lambda n: (n.gpm >= n.tgpm or (n.gpm / n.tgpm > 0.3)), codeList)
 
