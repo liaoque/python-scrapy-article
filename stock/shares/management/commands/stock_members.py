d40cf1747c965db5b34a9150ae60e13fc582cd30
mazuqiang <844596330@qq.com>
2022-06-02 11:15:05 +0800

    fh

--- stock/shares/management/commands/stock_members.py
+++ stock/shares/management/commands/stock_members.py
@@ -111,9 +111,9 @@ class Command(BaseCommand):
             shareName2 = SharesName.objects.filter(code=item.code_id)
             if len(shareName2) > 0:
                 if item.members >= item.tmembers:
-                    shareName2.update(member_up = 2)
+                    shareName2.update(member_up = 1)
                 else:
-                    shareName2.update(member_up=1)
+                    shareName2.update(member_up=2)
 
 
 
