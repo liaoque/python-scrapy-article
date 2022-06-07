5997c57e46934d8a141994a15758093a7a618b9f
mazuqiang <844596330@qq.com>
2022-06-07 22:45:58 +0800

    fh

--- stock/shares/management/commands/shares_block_top.py
+++ stock/shares/management/commands/shares_block_top.py
@@ -31,7 +31,7 @@ class Command(BaseCommand):
         """
         aggregate = {}
         aggregate_list = []
-        result = SharesName.objects.raw(sql, params=(date.today().strftime("%Y-%m-%d")))
+        result = SharesName.objects.raw(sql, params=(date.today().strftime("%Y-%m-%d"),))
         for item in result:
             if item.block_id not in list:
                 aggregate[item.block_code_id] = set()
@@ -47,4 +47,3 @@ class Command(BaseCommand):
                 l[key] = aggregate[one].intersection_update(aggregate[two], aggregate[three])
 
         print(l)
-
