d3a00792d118dc376f43d2a1508d6d398c804f39
mazuqiang <844596330@qq.com>
2022-06-08 01:04:52 +0800

    fh

--- stock/shares/management/commands/shares_block_top.py
+++ stock/shares/management/commands/shares_block_top.py
@@ -61,7 +61,10 @@ class Command(BaseCommand):
             # break
         l2 = {key: len(l[key]) for key in l if l[key] is not None}
         res = sorted(l2.items(),key=lambda d:d[1], reverse=True)
-        print(res[:10])
+        for item  in res[:10]:
+            print(item, res[item], l2[item])
+
+        # print(res[:10])
         # result = list(filter(lambda n: n.code_id in self.codeList, result))
         # l2 = {key: len(l[key]) for key in l if l[key] is not None}
         # print(l2)
