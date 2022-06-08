cdabba4ae78a26d224857aa2f8b3c96321dd3861
mazuqiang <844596330@qq.com>
2022-06-08 13:29:13 +0800

    fh

--- stock/shares/management/commands/stock_members.py
+++ stock/shares/management/commands/stock_members.py
@@ -103,7 +103,7 @@ class Command(BaseCommand):
             if len(sharesMembers) < 2:
                 continue
             sharesMembers = sharesMembers[:2]
-            if sharesMembers[1].members < sharesMembers[2].tmembers:
+            if sharesMembers[1].members < sharesMembers[2].members:
                 # 下降
                 item.update(member_up=1)
             else:
