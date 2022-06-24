2a946e15e848c4b84aa9944eac493e9a8b628f1c
mazuqiang <844596330@qq.com>
2022-06-24 15:38:01 +0800

    分红优质行业公司

--- stock/shares/management/commands/kdj-listen-predict.py
+++ stock/shares/management/commands/kdj-listen-predict.py
@@ -327,7 +327,7 @@ where ( n.gpm_ex > t.gpm_ex or  n.npmos_ex > t.npmos_ex)  and n.name not like %s
         heightBuy = SharesName.objects.filter(code=[item.code_id for item in send_data['buy']], npmos_ex__gte=4500)
         if len(heightBuy) > 0:
             str_con += "业绩好的：%s\n" % (
-                "\"\n\"".join([item.code_id for item in heightBuy])
+                "\"\n\"".join([item.code for item in heightBuy])
             )
 
         send_mail(
