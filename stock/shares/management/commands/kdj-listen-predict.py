fa19440e35a371138d151b6209e68f0ba888ac02
mazuqiang <844596330@qq.com>
2022-06-24 15:35:02 +0800

    分红优质行业公司

--- stock/shares/management/commands/kdj-listen-predict.py
+++ stock/shares/management/commands/kdj-listen-predict.py
@@ -326,7 +326,7 @@ where ( n.gpm_ex > t.gpm_ex or  n.npmos_ex > t.npmos_ex)  and n.name not like %s
 
         heightBuy = SharesName.objects.filter(code=[item.code_id for item in send_data['buy']], npmos_ex__gte=4500)
         if len(heightBuy) > 0:
-            str_con += "参考价格：%s\n" % (
+            str_con += "业绩好的：%s\n" % (
                 "\"\n\"".join([item.code_id for item in heightBuy])
             )
 
