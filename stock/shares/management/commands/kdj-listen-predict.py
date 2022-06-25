bd2b298a38c75a9e3fdc507c6de985b64779d358
mazuqiang <844596330@qq.com>
2022-06-26 00:24:18 +0800

    分红优质行业公司

--- stock/shares/management/commands/kdj-listen-predict.py
+++ stock/shares/management/commands/kdj-listen-predict.py
@@ -320,15 +320,16 @@ where ( n.gpm_ex > t.gpm_ex or  n.npmos_ex > t.npmos_ex)  and n.name not like %s
             "\",\"".join([item.code_id for item in send_data['buy']]),
             "\",\"".join([item.code_id for item in send_data['sell']]))
 
+        heightBuy = SharesName.objects.filter(code__in=[item.code_id for item in send_data['buy']], npmos_ex__gte=4500)
+        if len(heightBuy) > 0:
+            str_con += "业绩好的：%s\n" % (
+                "\,\"".join([item.code for item in heightBuy])
+            )
+
         str_con += "参考价格：%s\n" % (
             "\"\n\"".join([item.code_id + "：" + str(item.p_end) for item in send_data['buy']])
         )
 
-        heightBuy = SharesName.objects.filter(code__in=[item.code_id for item in send_data['buy']], npmos_ex__gte=4000)
-        if len(heightBuy) > 0:
-            str_con += "业绩好的：%s\n" % (
-                "\"\n\"".join([item.code for item in heightBuy])
-            )
 
         send_mail(
             '特别提醒%s' % (datetime.now(tz)),
