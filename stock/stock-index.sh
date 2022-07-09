f6f6cd33381ea93fbaa5e9a5fdad34b53983c5b3
mazuqiang <844596330@qq.com>
2022-07-10 00:47:53 +0800

    成交量

--- stock/stock-index.sh
+++ stock/stock-index.sh
@@ -3,6 +3,7 @@ export PATH=$PATH:/usr/local/bin
 source /alidata/python/venv/bin/activate
 cd /alidata/python/python-scrapy-article-master/stock && \
 python manage.py stock_index
-
+cd /alidata/python/python-scrapy-article-master/stock && \
+python manage.py stock_large_day
 
 ## 格雷厄姆指标， 每天一次提醒即可
