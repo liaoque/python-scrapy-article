ef408bf20e8c1247bd4ec73322ee6cc9ec583c2c
mazuqiang <844596330@qq.com>
2022-07-09 12:40:31 +0800

    格雷厄姆指数

--- stock/stock-index.sh
+++ stock/stock-index.sh
@@ -3,3 +3,6 @@ export PATH=$PATH:/usr/local/bin
 source /alidata/python/venv/bin/activate
 cd /alidata/python/python-scrapy-article-master/stock && \
 python manage.py stock_index
+
+
+## 格雷厄姆指标， 每天一次提醒即可
