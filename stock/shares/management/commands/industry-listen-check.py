79545cb100369d0107eaab7819abc40ef571c4b9
mazuqiang <844596330@qq.com>
2022-11-21 00:11:47 +0800

    支撑位板块检测

--- stock/shares/management/commands/industry-listen-check.py
+++ stock/shares/management/commands/industry-listen-check.py
@@ -1,24 +1,10 @@
 import numpy as np
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from django.core.management.base import BaseCommand, CommandError
-from django.db.models import Count, Max, Min
 
-# from ....polls.models import Question as Poll
 from shares.model.shares_name import SharesName
-from shares.model.shares_kdj import SharesKdj
-from shares.model.shares import Shares
-from shares.model.shares_hours import SharesHours
-from shares.model.shares_date_listen import SharesDateListen
-from shares.model.shares_macd import SharesMacd
-from shares.model.shares_industry_kdj import SharesIndustryKdj
-from shares.model.shares_industry_macd import SharesIndustryMacd
-from shares.model.shares_join_industry import SharesJoinIndustry
-from shares.model.shares_buys import SharesBuys
-from shares.model.shares_date import SharesDate
 import numpy as np
-import talib
-import math
-import sys
+from django.core.mail import send_mail
 from shares.model.shares_industry import SharesIndustry
 
 
@@ -31,76 +17,33 @@ from shares.model.shares_industry import SharesIndustry
 class Command(BaseCommand):
     help = '记录每天-10以下的kdj股票'
 
-    codeList = []
-    dateList = []
-    five_start = ''
-    twenty_start = ''
-    sixty_start = ''
-    one_hundred_start = ''
-    four_year_start = ''
-
-    date_as = None
-
-    def add_arguments(self, parser):
-        # parser.add_argument('poll_ids', nargs='+', type=int)
-        pass
 
     def handle(self, *args, **options):
+        send_data = {
+            'buy': [],
+            'sell': [],
+        }
         for item in SharesName.objects.filter(status=1, code_type=2):
             code = item.code
             sharesListSource = SharesIndustry.objects.filter(code_id=code).order_by('date_as')
-            i = 0
-            between = 0
-            up = True
-            sharesListSource = np.array(sharesListSource)
-            self.defMaxMin(sharesListSource, i + 1, up, between)
 
-    def defMaxMin(self, sharesListSource, i, up, between):
-        sharesList = sharesListSource[i * 10:10 + i * 10]
-        if len(sharesList) < 10:
-            return
-        lastIndex = len(sharesList) - 1
-        print(sharesList)
-        if up:
-            p_start = min(sharesList[0].p_end, sharesList[0].p_start)
-            if p_start > sharesList[lastIndex].p_end:
-                # 趋势反转， 到了压力位
-                between = maxIndustryIndex = self.max(sharesListSource, between, 10 + i * 10)
-                maxIndustry = sharesListSource[maxIndustryIndex]
-                maxIndustry.max_min_flag = -1
-                maxIndustry.save()
-                # sMaxIndustry = None
-                up = False
-            self.defMaxMin(sharesListSource, i + 1, up, between)
-        else:
-            p_start = max(sharesList[0].p_end, sharesList[0].p_start)
-            if p_start < sharesList[lastIndex].p_end:
-                # 趋势反转， 到了支撑位
-                between = minIndustryIndex = self.min(sharesListSource, between, 10 + i * 10)
-                minIndustry = sharesListSource[minIndustryIndex]
-                minIndustry.max_min_flag = 1
-                minIndustry.save()
-                # sMaxIndustry = None
-                up = True
-            self.defMaxMin(sharesListSource, i + 1, up, between)
+            sharesListSource3 = SharesIndustry.objects.filter(code_id=code, max_min_flag=1).order_by('date_as')
+            sharesListSource3 = np.array(sharesListSource3)[:3]
+            for item2 in sharesListSource3:
+                if abs(sharesListSource[0].avg_p_min_rate - item2.avg_p_min_rate) < .2:
+                    send_data['buy'].append(sharesListSource[0])
+                    break
 
-    def max(self, sharesList, start, end):
-        c = end
-        max2 = start
-        i = start
-        while i < c:
-            if sharesList[max2].p_end < sharesList[i].p_end:
-                max2 = i
-            i = i + 1
-        return max2
+        if len(send_data['buy']) > 0:
+            self.sendMessage(send_data['buy'])
 
-    def min(self, sharesList, start, end):
-        # sharesList = sharesList[start:end]
-        c = end
-        min2 = start
-        i = start
-        while i < c:
-            if sharesList[min2].p_end > sharesList[i].p_end:
-                min2 = i
-            i = i + 1
-        return min2
+    def sendMessage(self, send_data):
+        tz = timezone(timedelta(hours=+8))
+        str_con = "支撑位板块代码：%s\n" % ("\",\"".join(send_data['buy']))
+        send_mail(
+            '触碰支撑位%s' % (datetime.now(tz)),
+            str_con,
+            'lovemeand1314@163.com',
+            ['844596330@qq.com'],
+            fail_silently=False,
+        )
