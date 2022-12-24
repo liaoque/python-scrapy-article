12df7896ca0a060238babe3ee05d7102fb7d0e90
mazuqiang <844596330@qq.com>
2022-12-24 13:16:55 +0800

    week

--- stock/shares/management/commands/industry-listen-check.py
+++ stock/shares/management/commands/industry-listen-check.py
@@ -7,6 +7,7 @@ import numpy as np
 from django.core.mail import send_mail
 from shares.model.shares_industry import SharesIndustry
 from shares.model.shares_industry_macd import SharesIndustryMacd
+from shares.model.shares_industry_week import SharesIndustryWeek
 
 # 校验的
 # 1. 检测当天板块 macd是否上升
@@ -34,6 +35,15 @@ class Command(BaseCommand):
                         and sharesIndustryMacd[0].macd >= 0:
                     send_data['buy'].append(sharesListSource[0].code_id)
                     break
+
+            sharesListSource3 = SharesIndustryWeek.objects.filter(code_id=code, max_min_flag=1).order_by('-date_as')
+            sharesListSource3 = np.array(sharesListSource3)[:3]
+            for item2 in sharesListSource3:
+                sharesListSource = SharesIndustryWeek.objects.filter(code_id=code).order_by('-date_as')
+                if abs(sharesListSource[0].avg_p_min_rate - item2.avg_p_min_rate) < .2 :
+                    send_data['buy'].append(sharesListSource[0].code_id)
+                    break
+
         if len(send_data['buy']) > 0:
             self.sendMessage(send_data)
 
