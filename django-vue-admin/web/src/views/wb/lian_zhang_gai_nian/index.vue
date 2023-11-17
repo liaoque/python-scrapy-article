2d48f963d393c28de7de1f647bc717c33e683f30
mzq <mazuqiang@lane.com>
2023-11-17 16:56:37 +0800

    重构界面

--- django-vue-admin/web/src/views/wb/lian_zhang_gai_nian/index.vue
+++ django-vue-admin/web/src/views/wb/lian_zhang_gai_nian/index.vue
@@ -28,7 +28,7 @@ import * as api from './api'
 import { crudOptions } from './crud'
 import { d2CrudPlus } from 'd2-crud-plus'
 export default {
-  name: 'yuan_yin',
+  name: 'lian_zhang_gai_nian',
   mixins: [d2CrudPlus.crud],
   data () {
     return {
