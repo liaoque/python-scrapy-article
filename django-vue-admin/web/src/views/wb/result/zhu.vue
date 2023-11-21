aa34a539cb47d9c0a91229e0959ecb59d69a5899
mzq <mazuqiang@lane.com>
2023-11-21 16:03:44 +0800

    t

--- django-vue-admin/web/src/views/wb/result/zhu.vue
+++ django-vue-admin/web/src/views/wb/result/zhu.vue
@@ -8,12 +8,21 @@
       </el-table-column>
       <el-table-column prop="jingjiajinetoday_s" sortable sort-by="jingjiajinetoday" label="竞价未匹配">
       </el-table-column>
-      <el-table-column prop="suoshugainian" sortable sort-by="suoshugainian" label="所属概念">
+      <el-table-column sortable sort-by="suoshugainian" label="所属概念">
+        <template slot-scope="scope">
+          <el-popover trigger="hover" placement="top">
+            <el-tag disable-transitions :key="index"
+              v-for="(gainian, index) in scope.row.suoshugainian">{{ gainian }}</el-tag>
+            <div slot="reference" class="name-wrapper">
+              <el-tag size="medium">{{ scope.row.suoshugainian.join(",").slice(0, 10) }}</el-tag>
+            </div>
+          </el-popover>
+        </template>
       </el-table-column>
 
       <el-table-column prop="zhangdiefuqianfuquantoday_s" sortable sort-by="zhangdiefuqianfuquantoday" label="涨跌幅">
       </el-table-column>
-      <el-table-column prop="zhangdie4thday_s" sortable sort-by="zhangdie4thday" label="4日涨跌幅">
+      <el-table-column prop="zhangdie4thday_s" sortable sort-by="zhangdie4thday" label="4日涨跌">
       </el-table-column>
       <el-table-column prop="jingjiajinechengjiaoliangbi_s" sortable sort-by="jingjiajinechengjiaoliangbi" label="竞价量比">
       </el-table-column>
@@ -21,20 +30,19 @@
       </el-table-column>
       <el-table-column prop="lianbantianshuyesterday" sortable sort-by="lianbantianshuyesterday" label="昨日连板天数">
       </el-table-column>
-      <el-table-column prop="zhangfu120_s" sortable sort-by="zhangfu120" label="120日涨跌幅">
+      <el-table-column prop="zhangfu120_s" sortable sort-by="zhangfu120" label="120日涨跌">
       </el-table-column>
-      <el-table-column prop="yidongcishu" sortable sort-by="yidongcishu" label="异动次数">
+      <el-table-column prop="yidongcishu" sortable sort-by="yidongcishu" label="异动">
       </el-table-column>
       <el-table-column prop="jianguanleixingyesterday" sortable sort-by="jianguanleixingyesterday" label="监管">
       </el-table-column>
       <el-table-column prop="chuangbairixingao" sortable sort-by="chuangbairixingao" label="创百日新高">
       </el-table-column>
-      <el-table-column prop="ziyouliutongshizhiyesterday_s" sortable sort-by="ziyouliutongshizhiyesterday" label="自由流通市值">
+      <el-table-column prop="ziyouliutongshizhiyesterday_s" sortable sort-by="ziyouliutongshizhiyesterday" label="流通市值">
       </el-table-column>
     </el-table>
     <div class="block">
-      <el-pagination layout="prev, pager, next"  :total="data.length" :current-page="currentPage"
-         :page-size="pageSize"
+      <el-pagination layout="prev, pager, next" :total="data.length" :current-page="currentPage" :page-size="pageSize"
         @current-change="handleCurrentChange">
       </el-pagination>
     </div>
@@ -66,10 +74,33 @@ export default {
     },
     cellClassName (row) {
       const key = 'color' + row.columnIndex
+      // '    紫色 29 橘色 46 黄色 36 绿色 35 红色 3  粉色 13551615
       if (parseInt(row.row[key]) === 35) {
         return 'el-button--success'
+      }
+      if (row.columnIndex === 0 && parseInt(row.row[key]) === 46) {
+        return 'el-button--46'
+      } else if (row.columnIndex === 1 && parseInt(row.row[key]) === 36) {
+        return 'el-button--36'
+      } else if (row.columnIndex === 2 && parseInt(row.row[key]) === 29) {
+        return 'el-button--29'
       }
     }
   }
 }
 </script>
+
+<style>
+.el-table {
+  font-size: 10px;
+}
+.el-button--29{
+  
+}
+.el-button--36{
+  
+}
+.el-button--46{
+  
+}
+</style>
