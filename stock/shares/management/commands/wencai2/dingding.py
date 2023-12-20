f19c6cd263e314fc89450ee633513aa539fb5d08
mazuqiang <844596330@qq.com>
2023-12-21 01:36:13 +0800

    bug

--- stock/shares/management/commands/wencai2/dingding.py
+++ stock/shares/management/commands/wencai2/dingding.py
@@ -6,14 +6,16 @@ def dingding(s):
     # s = "半年报预增，所属概念，s去除ST，去除北交所，去除新股"
     url = 'https://oapi.dingtalk.com/robot/send?access_token=f51bee16742b25506373378cd7a33def2c1ce7d253998c59bfbdff28ffaf15d5'
 
-    data = {
+    data = """
+    {
         'msgtype': 'text',
         'text': {
-            "content":s
+            'content':%s
         }
     }
+    """%(s)
     headers = {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
     }
     response = requests.post(url, data=data, headers=headers)
