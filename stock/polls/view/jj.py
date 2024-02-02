047cf53c66a4983fc866b853a84216910f2b70e1
mazuqiang <844596330@qq.com>
2024-02-03 00:59:34 +0800

    基金

--- stock/polls/view/jj.py
+++ stock/polls/view/jj.py
@@ -1,7 +1,9 @@
 
+from django.http import HttpResponse, Http404, HttpResponseRedirect, JsonResponse
+from django.template import loader
 import requests
 
 def jj_data(request, code):
     url = "https://fund.10jqka.com.cn/ifindRank/commonTypeAvgFqNet/%s.json"%(code)
     r = requests.get(url)
-    return r.json()
+    return JsonResponse(r.json())
