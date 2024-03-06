1cf9d6821ca9d27a111714065538098f0536df7c
mzq <mazuqiang@lane.com>
2024-03-06 19:27:46 +0800

    现在源头加一个涨停原因表的首板

--- django-vue-admin/backend/dvadmin/wb/core/pi_pei_gai_nian.py
+++ django-vue-admin/backend/dvadmin/wb/core/pi_pei_gai_nian.py
@@ -1,4 +1,5 @@
 from dvadmin.wb.config import code_config
+from dvadmin.wb.utils import gp
 
 """
 Sub 匹配概念()
@@ -514,13 +515,13 @@ def definedcolor1(chuang_data, bu_zhang_data, qx, is_chuang_ye=1):
         if item["jianguanleixingyesterday"] != "" or \
                 item["jingjiajinejingjialiangbi"] > 700 or \
                 item["jingjiajinechengjiaoliangbi"] >= jingjiajinechengjiaoliangbi or \
-                (is_chuang_ye != 1 and item["ziyouliutongshizhiyesterday"] / 10000 / 10000 > 100 and item["code"] !=
+                (is_chuang_ye != 1 and item["ziyouliutongshizhiyesterday"] / 10000 / 10000 > 100 and gp.getCode(item["code"]) !=
                  bu_zhang_data["zhong_jun"]["code"]):
             item["color1"] = 35
 
         if qx == -1:
             if item["jingjiajinechengjiaoliangbi"] >= 0.08 and item["zhangdiefuqianfuquantoday"] < 0 and item["code"] != \
-                    bu_zhang_data["lian_ban_code"]["code"]:
+                    bu_zhang_data["lian_ban_code"]["code"] :
                 item["color1"] = 35
                 item["color4"] = 35
 
