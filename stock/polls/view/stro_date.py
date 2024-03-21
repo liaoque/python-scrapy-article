ade2186e7ca20e6579854661c2f7fcaccfe15d0e
mzq <mazuqiang@lane.com>
2024-03-21 12:56:09 +0800

    再增加封单控制

--- stock/polls/view/stro_date.py
+++ stock/polls/view/stro_date.py
@@ -4,8 +4,9 @@ import requests
 import re
 import json
 from ..model.shares_date_listen import SharesDateListen
+from django.views.decorators.csrf import csrf_exempt
 
-
+@csrf_exempt
 def stro_date(request):
     response = JsonResponse({}, safe=False)
     if request.method == 'POST':
@@ -13,32 +14,40 @@ def stro_date(request):
         data = json.loads(request.body)
         # 处理数据，例如保存到数据库等
         # ...
-        # for item in data["data"]["z"]["yellow"]:
-        #     transformed_data = SharesDateListen.objects.filter(date_as=data["d"], code=item["code"],type=5)
-        #     if len(transformed_data) > 0:
-        #         continue
-        #     SharesDateListen(
-        #         buy_date_as=data["d"],
-        #         date_as=data["d"],
-        #         code=data["code"],
-        #         p_start=0,
-        #         buy_pre=0,
-        #         type=5
-        #     )
-        #     pass
-        # for item in data["data"]["z"]["orange"]:
-        #     pass
-        # for item in data["data"]["z"]["purple"]:
-        #     pass
-        # for item in data["data"]["c"]["yellow"]:
-        #     pass
-        # for item in data["data"]["c"]["orange"]:
-        #     pass
-        # for item in data["data"]["c"]["purple"]:
-        #     pass
-
+        for code in data["data"]["z"]["yellow"]:
+            saveCode(code, data, 5)
+            pass
+        for code in data["data"]["z"]["orange"]:
+            saveCode(code, data, 6)
+            pass
+        for code in data["data"]["z"]["purple"]:
+            saveCode(code, data, 7)
+            pass
+        for code in data["data"]["c"]["yellow"]:
+            saveCode(code, data, 5)
+            pass
+        for code in data["data"]["c"]["orange"]:
+            saveCode(code, data, 6)
+            pass
+        for code in data["data"]["c"]["purple"]:
+            saveCode(code, data, 7)
+            pass
 
         response = JsonResponse(data, safe=False)
 
     response["Access-Control-Allow-Origin"] = "*"
     return response
+
+
+def saveCode(code, data, type):
+    transformed_data = SharesDateListen.objects.filter(date_as=data["d"], code=code, type=type)
+    if len(transformed_data) > 0:
+        return
+    SharesDateListen(
+        buy_date_as=data["d"],
+        date_as=data["d"],
+        code=code,
+        p_start=0,
+        buy_pre=0,
+        type=type
+    )
