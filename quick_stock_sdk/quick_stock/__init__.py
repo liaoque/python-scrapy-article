64accc357eab621963ee2501750b5493698ac391
mzq <mazuqiang@lane.com>
2024-07-10 17:19:16 +0800

    init

--- quick_stock_sdk/quick_stock/__init__.py
+++ quick_stock_sdk/quick_stock/__init__.py
@@ -3,8 +3,6 @@
 from .trade_date_client import TradeDateClient
 from .trade_index_client import TradeIndexClient
 from .trade_stock_client import TradeStockClient
+from .trade_concept_client import TradeConceptClient
 
-__all__ = ["TradeDateClient", "TradeIndexClient","TradeStockClient"]
-
-
-
+__all__ = ["TradeDateClient", "TradeIndexClient","TradeStockClient", "TradeConceptClient"]
--- test/test.py
+++ test/test.py
@@ -6,6 +6,8 @@ import quick_stock as quick_stock
 import talib
 import matplotlib.pyplot as plt
 from mplfinance.original_flavor import candlestick_ohlc
+
+
 def macd(close_prices):
     # 计算kd指标
     macdDIFF, macdDEA, macd = talib.MACDEXT(close_prices, fastperiod=12, fastmatype=1, slowperiod=26,
@@ -15,21 +17,26 @@ def macd(close_prices):
     return (macdDIFF, macdDEA, macd)
 
 
-def compute(title,code):
+def compute(title, code):
     sz50 = quick_stock.TradeIndexClient().daily(code)  # 深证50
     sz50 = pd.DataFrame(sz50)
     mac, signal, hist = macd(sz50["end"])
     return {
-        "title": code,
+        "title": title,
         "data": sz50,
-        "macd": macd,
+        "macd": mac,
         "signal": signal,
         "hist": hist,
     }
-def draw(title, fig, code, i):
-    sz50 = quick_stock.TradeIndexClient().daily(code)  # 深证50
-    sz50 = pd.DataFrame(sz50)
-    mac, signal, hist = macd(sz50["end"])
+
+
+def draw(data, fig, i):
+    # sz50 = quick_stock.TradeIndexClient().daily(code)  # 深证50
+    sz50 = pd.DataFrame(data["data"])
+    # mac, signal, hist = macd(sz50["end"])
+    mac = data["macd"]
+    hist = data["hist"]
+    signal = data["signal"]
 
     dates = sz50["date_at"][-120:]
     open_prices = sz50["start"][-120:]
@@ -41,7 +48,7 @@ def draw(title, fig, code, i):
     candlestick_ohlc(ax1, zip(np.arange(1, len(dates)), open_prices, high_prices, low_prices, close_prices), width=0.6,
                      colorup='g',
                      colordown='r')
-    ax1.set_title(title)
+    ax1.set_title(data["title"])
     ax1.set_ylabel('price')
 
     ax2 = fig.add_subplot(7, 2, i)
@@ -59,93 +66,32 @@ def draw(title, fig, code, i):
     ax3.plot(dates, signal, color='red', label='Histogram')
     ax4 = ax2.twinx()
     ax4.plot(dates, hist, color='#339958', label='Histogram')
-    return fig,i
+    return fig, i
 
 
 if __name__ == '__main__':
     # indexes = quick_stock.TradeIndexClient().get_all_index()
     # print(indexes[indexes[""] ==""][:20])
-
-    data = []
-    data.append(compute("sz50", "0.399850"))
-    data.append(compute("a50", "1.000016"))
-    data.append(compute("kc50", "1.000688"))
-    data.append(compute("hs300", "1.000300"))
-    data.append(compute("zz500", "1.000905"))
-    data.append(compute("zz1000", "1.000852"))
-    data.append(compute("zz2000", "2.932000"))
-
-    # for i range 10:
     #
+    # data = []
+    # data.append(compute("sz50", "0.399850"))
+    # data.append(compute("a50", "1.000016"))
+    # data.append(compute("kc50", "1.000688"))
+    # data.append(compute("hs300", "1.000300"))
+    # data.append(compute("zz500", "1.000905"))
+    # data.append(compute("zz1000", "1.000852"))
+    # data.append(compute("zz2000", "2.932000"))
+    #
+    # fig = plt.figure(figsize=(20, 16), )
     # i = 1
-    fig = plt.figure(figsize=(20, 16),)
-    fig,i = draw('sz50',fig, "0.399850", i)
-    fig,i = draw('a50',fig, "1.000016", i)
-    fig,i = draw('kc50',fig, "1.000688", i)
-    fig,i = draw('hs300',fig, "1.000300", i)
-    fig,i = draw('zz500',fig, "1.000905", i)
-    fig,i = draw('zz1000',fig, "1.000852", i)
-    fig,i = draw('zz2000',fig, "2.932000", i)
-    plt.show()
-    # sz50 = quick_stock.TradeIndexClient().daily("0.399850") # 深证50
-    # sz50 = pd.DataFrame(sz50)
-    # macd, signal, hist = macd(sz50["end"])
-
-    # sz50 = quick_stock.TradeIndexClient().daily("1.000016") # 上证50
-    # macd, signal, hist = macd(sz50["end"])
-    #
-    # kc50 = quick_stock.TradeIndexClient().daily("1.000688") # 科创50
-    # macd, signal, hist = macd(kc50["end"])
-    #
-    # hs300 = quick_stock.TradeIndexClient().daily("1.000300") # 沪深300
-    # macd, signal, hist = macd(hs300["end"])
-    #
-    # zz500 = quick_stock.TradeIndexClient().daily("1.000905") # 中证500
-    # macd, signal, hist = macd(zz500["end"])
-    #
-    # zz1000 = quick_stock.TradeIndexClient().daily("1.000852") # 中证1000
-    # macd, signal, hist = macd(zz1000["end"])
-    #
-    # zz2000 = quick_stock.TradeIndexClient().daily("2.932000") # 中证2000
-    # macd, signal, hist = macd(zz2000["end"])
-
-    # dates = sz50["date_at"][-120:]
-    # open_prices = sz50["start"][-120:]
-    # high_prices = sz50["max"][-120:]
-    # low_prices = sz50["min"][-120:]
-    # close_prices = sz50["end"][-120:]
-    # fig, (ax1, ax2) = plt.subplots(2, sharex=True, figsize=(10, 8))
-    # candlestick_ohlc(ax1, zip(np.arange(1, len(dates)) , open_prices, high_prices, low_prices, close_prices), width=0.6, colorup='g',
-    #                  colordown='r')
-    # ax1.set_title('sz50')
-    # ax1.set_ylabel('price')
-    #
-    # # 绘制MAC柱状图
-    # macd = macd[-120:]
-    # signal = signal[-120:]
-    # hist = hist[-120:]
-    # ax2.bar(dates, macd, color='#f059c6')
-    # ax2.set_title('macd')
-    # ax2.set_xlabel('date')
-    # ax2.set_ylabel('macd')
-    #
-    # ax3 = ax2.twinx()
-    # ax3.plot(dates, signal, color='red', label='Histogram')
-    # ax4 = ax2.twinx()
-    # ax4.plot(dates, hist, color='#339958', label='Histogram')
-
-
-
-    # fig, ax1 = plt.subplots()
-    # d = sz50['date_at'][-10:]
-    # e = sz50['end'][-10:]
-    # ax1.plot(d, e, color='blue', label='Close')
-    # ax1.set_ylabel('Price')
-    #
-    # ax2 = ax1.twinx()
-    # e = hist[-10:]
-    # ax2.bar(d, e, color='red', label='Histogram')
-    # ax2.set_ylabel('MACD')
-    #
-    # fig.legend(loc='upper left')
+    # for item in data:
+    #     fig, i = draw(item, fig, i)
     # plt.show()
+
+    all_concept = quick_stock.TradeConceptClient().get_all_concept()
+
+    for item in all_concept["code"]:
+        # print(item)
+        df = quick_stock.TradeConceptClient().get_all_concept_stock(item)
+
+    print(11)
