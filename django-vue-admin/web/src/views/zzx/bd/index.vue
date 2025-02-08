7927e6a29fab6cb3cf2504d8b090cf16e45a3b21
mazuqiang <844596330@qq.com>
2025-02-08 21:01:35 +0800

    init

--- django-vue-admin/web/src/views/zzx/bd/index.vue
+++ django-vue-admin/web/src/views/zzx/bd/index.vue
@@ -1,21 +1,112 @@
 <template>
   <d2-container :class="{ 'page-compact': crud.pageOptions.compact }">
     <el-row>
-      <el-col :span="12">
+      <el-col :span="24">
         <el-row>
           <el-col :span="24/left.bd.length"    v-for="(bd,index) in left.bd" :key="index" >
-              <p >{{bd.title}}</p>
-              <p v-for="(item,index2) in bd.data" :key="index2">{{item.name}}</p>
+            <el-table
+              :data="bd.data"
+              style="width: 100%">
+              <el-table-column
+                :label="bd.title"
+                width="180">
+                <template slot-scope="scope">
+                  <i >{{ scope.$index + 1 }}</i>
+                  <span style="margin-left: 10px" @click="selectBd(bd, index)" >{{ scope.row.name }}</span>
+                </template>
+              </el-table-column>
+            </el-table>
           </el-col>
         </el-row>
       </el-col>
-      <el-col :span="12" >
-        <div class="grid-content bg-purple-light"></div>
-      </el-col>
+<!--      <el-col :span="12" >-->
+<!--        <div class="grid-content bg-purple-light"></div>-->
+<!--      </el-col>-->
     </el-row>
+
+    <el-drawer
+      :title="right.title"
+      :visible.sync="right.drawer"
+      :direction="right.direction"
+      size="70%"
+     >
+      <el-table
+        default-expand-all="true"
+        :data="right.data"
+        style="width: 100%">
+        <el-table-column
+          type="index"
+          label="人气"
+          width="50">
+        </el-table-column>
+        <el-table-column
+          prop="code"
+          label="代码"
+          width="60">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="名称"
+          width="100">
+        </el-table-column>
+        <el-table-column
+          prop="fudu"
+          label="涨幅"
+          width="60">
+        </el-table-column>
+        <el-table-column
+          prop="rank"
+          label="总分"
+          width="60">
+        </el-table-column>
+        <el-table-column
+          prop="change_rank"
+          label="升降"
+          width="60">
+          <template slot-scope="scope">
+            <span >{{scope.row.change_rank}}</span>
+            <i :class='{
+              "el-icon-top": parseInt(scope.row.change_rank) > 0,
+             "el-icon-bottom": parseInt(scope.row.change_rank) < 0,
+            }'
+            :style="{ color: parseInt(scope.row.change_rank) > 0 ? '#F56C6C' : (parseInt(scope.row.change_rank) === 0 ? '': '#67C23A')}"></i>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="bk"
+          label="板块"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="riseDay"
+          label="版状态"
+          width="60">
+        </el-table-column>
+        <el-table-column
+          label="题材"
+          type="expand">
+          <template slot-scope="scope">
+            <div>{{scope.row.reason}}</div>
+          </template>
+        </el-table-column>
+
+
+      </el-table>
+    </el-drawer>
 
   </d2-container>
 </template>
+
+
+<style scoped>
+.green{
+  color: #67C23A;
+}
+.red{
+  color: #F56C6C;
+}
+</style>
+
 
 <script>
 import * as api from './api'
@@ -25,6 +116,13 @@ export default {
   mixins: [d2CrudPlus.crud],
   data () {
     return {
+      right:{
+        index: -1,
+        drawer: false,
+        direction: 'rtl',
+        title: '',
+        data:[]
+      },
       left: {
         bd: [
           {
@@ -54,6 +152,13 @@ export default {
     // ShouBan: () => import('./shou_ban.vue') // 这里填写实际的组件路径
   },
   methods: {
+    selectBd(bg, index) {
+      this.right.drawer = true
+      this.right.index = index
+      this.right.title = bg.title
+      this.right.data = bg.data
+      console.log(bg, index)
+    },
     getToday () {
       const today = new Date()
       const year = today.getFullYear()
@@ -109,3 +214,4 @@ export default {
   }
 }
 </script>
+
