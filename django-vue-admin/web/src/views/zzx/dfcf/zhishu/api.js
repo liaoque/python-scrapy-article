cb4cd3dbdce84da62f1f9c962279f98110178b42
mazuqiang <844596330@qq.com>
2025-03-12 01:29:48 +0800

    缠论

--- django-vue-admin/web/src/views/zzx/dfcf/zhishu/api.js
+++ django-vue-admin/web/src/views/zzx/dfcf/zhishu/api.js
@@ -1,4 +1,5 @@
 import { request } from '@/api/service'
+import {response} from "@/api/tools";
 
 /**
  * 国债利率
@@ -40,41 +41,8 @@ export function GetEtf1000QqList (date) {
   })
 }
 
-
-
 export function GetList (code) {
-  let prefix = '0.'
-  const code3 = code.substr(0,3)
-  if (code3 === '688'){
-    prefix = '1.'
-  } else if (code3 === '000') {
-    prefix = '0.'
-  } else if (code3 === '002') {
-    prefix = '0.'
-  } else if (code3 === '300') {
-    prefix = '0.'
-  } else if (code3.substr(0, 2) === '60'){
-    prefix = '1.'
-  }
-
-  return request({
-    url: 'https://push2his.eastmoney.com/api/qt/stock/kline/get',
-    method: 'get',
-    params: {
-      code: code,
-      cb: '',
-      secid: prefix + code,
-      ut: '',
-      fields1: 'f1,f2,f3,f4,f5,f6',
-      fields2: 'f51,f52,f53,f54,f55,f56,f57,f58,f59,f60,f61',
-      klt: 101,
-      fqt: 1,
-      end: '20500101',
-      lmt: '250',
-      _: 1739621395911
-    // code: "600839",
-    }
-  })
+  return response({})
 }
 
 export function AddObj (obj) {
@@ -106,18 +74,4 @@ export function GetCascadeData () {
   })
 }
 
-export function GetYuanYin (today, fd, yd) {
-  return request({
-    url: '/api/wb/yuan_yin?current_time=' + today + '&fd=' + fd + '&yd=' + yd,
-    method: 'get',
-    data: { current_time: today }
-  })
-}
 
-export function GetChanLun (code) {
-  return request({
-    url: 'http://192.168.112.57:30080/api/lh/chanlun',
-    method: 'get',
-    params: { code: code }
-  })
-}
