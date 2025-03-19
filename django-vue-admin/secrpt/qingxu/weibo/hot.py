43ae3c2971ffb2d11fe18571d27a464d024aafb7
mazuqiang <844596330@qq.com>
2025-03-19 22:43:56 +0800

    hl

--- django-vue-admin/secrpt/qingxu/weibo/hot.py
+++ django-vue-admin/secrpt/qingxu/weibo/hot.py
@@ -1,17 +1,19 @@
+from urllib.parse import urlencode
+
 import requests
+import re
 
 def fetch_weibo_cardlist(api_url, headers=None, params=None):
     # 如果未提供 headers，则使用默认 headers
     if headers is None:
         headers = {
-            "Authorization": "WB-SUT _2A95K3CEXDeRxGeVL7FsT8irMyD2IHXVnyDPfrDV6PUJbkdANLWn7kWpNTD9Yt3tLUsvIradO2GvMEYkO9OJ4q-3P",
-            "Host": "api.weibo.cn",
+            "User-Agent": "SM-G9810_7.1.2_weibo_11.6.0_android",
             "Connection": "Keep-Alive",
             "Accept-Encoding": "gzip"
         }
 
     # 发送HTTP GET请求
-    response = requests.get(api_url, headers=headers, params=params)
+    response = requests.get(api_url, )
 
     # 检查响应状态码是否为200
     if response.status_code == 200:
@@ -21,56 +23,45 @@ def fetch_weibo_cardlist(api_url, headers=None, params=None):
         # 返回错误信息
         return f"Failed to fetch data: {response.status_code}"
 
-# 使用示例
-api_url = "https://api.weibo.cn/2/cardlist"
 
-# 定义参数字典
-params = {
-    "networktype": "wifi",
-    "image_type": "heif",
-    "launchid": "10000365--x",
-    "orifid": "5$$$0$$5",
-    "uicode": "10000327",
-    "ul_hid": "7d9bdb52-fe84-43ff-b9a4-4e9daea3df45",
-    "ul_sid": "7d9bdb52-fe84-43ff-b9a4-4e9daea3df45",
-    "moduleID": "708",
-    "featurecode": "10000084",
-    "wb_version": "5017",
-    "card159164_emoji_enable": "1",
-    "lcardid": "top_icon_category_all_1288",
-    "c": "android",
-    "s": "3d54939f",
-    "ft": "0",
-    "ua": "samsung-SM-G9810__weibo__11.6.0__android__android7.1.2",
-    "wm": "2468_1001",
-    "aid": "01A9CZ5vwg8-qUABOuifv9KYpG2ytFN9_HqyvN6pBHq19545s.",
-    "fid": "230771_-_Finance_Cardlist_Finder",
-    "uid": "3579224031",
-    "v_f": "2",
-    "v_p": "89",
-    "from": "10B6095010",
-    "gsid": "_2A25K3CEXDeRxGeVL7FsT8irMyD2IHXVnyDPfrDV6PUJbkdANLXf8kWpNTD9Ytyq_95zJqClQt6xGa4geCz",
-    "imsi": "460075639459304",
-    "lang": "zh_CN",
-    "lfid": "5",
-    "page": "1",
-    "skin": "default",
-    "count": "20",
-    "oldwm": "2468_1001",
-    "sflag": "1",
-    "oriuicode": "10000495_0_10000495_0_10000495",
-    "containerid": "230771_-_Finance_Cardlist_Finder",
-    "ignore_inturrpted_error": "true",
-    "luicode": "10000495",
-    "android_id": "813a2130fd1b9da8",
-    "client_key": "d41d8cd98f00b204e9800998ecf8427e",
-    "show_layer": "1",
-    "need_new_pop": "1",
-    "ul_ctime": "1742231492528",
-    "need_head_cards": "0",
-    "cum": "E5C1F2EE"
-}
 
-# 调用函数并打印结果
-result = fetch_weibo_cardlist(api_url, params=params)
-print(result)
+
+if __name__ == '__main__':
+    # 使用示例
+    api_url = "https://api.weibo.cn/2/cardlist"
+
+    # 定义参数字典
+    params = {
+        "c": "android",
+        "s": "3d54939f",
+        "from": "10B6095010",
+        "gsid": "_2A25K3CEXDeRxGeVL7FsT8irMyD2IHXVnyDPfrDV6PUJbkdANLXf8kWpNTD9Ytyq_941zqq95zJqClQt6xGa4geCz",
+        "containerid": "230771_-_Finance_Cardlist_Finder",
+    }
+    query_string = urlencode(params)
+    api_url = f"{api_url}?{query_string}"
+    # 调用函数并打印结果
+    result = fetch_weibo_cardlist(api_url,)
+    ts = [{
+        'id':item['mblog']['id'],
+        'user':{
+            'user_id' :item['mblog']['user']['id'],
+            'name' :item['mblog']['user']['name'],              #名字
+            'verified' :item['mblog']['user']['verified_reason'],       #认证信息
+            'followers_count' :item['mblog']['user']['followers_count'],    # 粉丝数量
+            'verified_level' :item['mblog']['user'].get('verified_level',0),    # 粉丝数量
+            'verified_type_ext' :item['mblog']['user'].get('verified_type_ext',''),    # 粉丝数量
+        },
+
+        'content':item['mblog']['text'],
+        'created_at':item['mblog']['created_at'],
+        'metrics':{
+          "likes":  item['mblog']['multi_attitude'],    #  点赞数
+          "reposts":  item['mblog']['multi_attitude'],  #   转发数
+          "comments":  item['mblog']['multi_attitude'], #   评论数
+        },
+        'hashtags': re.findall(r'#[^#]+#', item['mblog']['text']),
+        'media_urls': item['mblog']['pic_ids'],
+        'location':item['mblog'].get('region_name', '').replace('发布于 ', ""),    #地址
+    } for item in result['cards'] if item['card_type'] == 9]
+    print(ts)
