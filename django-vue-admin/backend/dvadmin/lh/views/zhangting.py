ccdd98997f9b1649eebfc88a59b4885db4d4f99d
mazuqiang <844596330@qq.com>
2025-06-15 18:49:57 +0800

    yq

--- django-vue-admin/backend/dvadmin/lh/views/zhangting.py
+++ django-vue-admin/backend/dvadmin/lh/views/zhangting.py
@@ -67,34 +67,8 @@ class ZhangTingView(View):
         })
 
     def post(self, request, *args, **kwargs):
-        body_data = json.loads(request.body)
-        current_time = body_data.get('current_time')
-        data = body_data.get('data')
-        if current_time is None or current_time == "":
-            return JsonResponse({"error": "current_time must"})
-        table = self.db['yq']  # 选择你的数据
-        res = table.find_one({
-            "current_time": current_time
-        }, {"_id": 1})
-
-        # 如果数据存在则更新， 不存在则创建
-        if res:
-            # 如果数据存在则更新
-            table.update_one(
-                {"current_time": current_time},
-                {"$set": {"data": data}}
-            )
-            message = "Data updated successfully."
-        else:
-            # 如果数据不存在则创建
-            table.insert_one({
-                "current_time": current_time,
-                "data": data
-            })
-            message = "Data created successfully."
-
         return JsonResponse({
             "code": 2000,
-            "msg": message,
+            "msg": "success",
             "data": {}
         })
